746c7391d38fc7b0eead8e7e696527450cf81a8b
Meital Levy <levymeit@gmail.com>
2010-12-05 05:58:50 +0000

    PseudorandomGenerator interface
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@69 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/src/edu/biu/scapi/primitives/crypto/prg/PseudorandomGenerator.java
+++ Code/src/edu/biu/scapi/primitives/crypto/prg/PseudorandomGenerator.java
@@ -11,42 +11,45 @@ import java.security.spec.KeySpec;
   */
 public interface PseudorandomGenerator {
 	/** 
-	 * @param secretKey
+	 * Initializes this prg with the secret key
+	 * @param secretKey - the secrete key
 	 */
 	public void init(KeySpec secretKey);
 
 	/** 
+	 * Initializes this prg with the secret key and the auxiliary parameters
 	 * @param secretKey
 	 * @param params
 	 */
 	public void init(KeySpec secretKey, AlgorithmParameterSpec params);
 
 	/** 
-	 * @return
+	 * @return the parameter spec of this prg
 	 */
 	public AlgorithmParameterSpec getParams();
 
 	/** 
+	 * The algorithm name
 	 */
 	public void getAlgorithmName();
 
 	/** 
-	 * @param _byte
+	 * @param bytein - the single byte to xor
 	 */
 	public void streamSingleByte(byte bytein);
 
 	/** 
-	 * @param bytein_bytes
-	 * @param intinOff
-	 * @param intlen
-	 * @param intoutOff
+	 * @param in_bytes - the input bytes
+	 * @param inOff - input offset
+	 * @param len - length
+	 * @param outOff - 
 	 * @param byteout_bytes
 	 */
 	public void streamBytes(byte[] in_bytes, int inOff,
 			int len, int outOff, byte[] out_bytes);
 
 	/** 
-	 * @return
+	 * @return the secret key
 	 */
 	public KeySpec getSecretKeySpec();
 }
