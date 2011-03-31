87a9a95a443551c6ef5af6e72a30c3a7454109c6
Meital Levy <levymeit@gmail.com>
2011-03-31 11:11:04 +0000

    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@126 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/src/edu/biu/scapi/primitives/crypto/prf/PseudorandomFunction.java
+++ Code/src/edu/biu/scapi/primitives/crypto/prf/PseudorandomFunction.java
@@ -1,10 +1,15 @@
 /**
- * 
+ * Pseudorandom function definition: In cryptography, a pseudorandom function family, abbreviated PRF, 
+ * is a collection of efficiently-computable functions which emulate a random oracle in the following way: 
+ * no efficient algorithm can distinguish (with significant advantage) between a function chosen randomly from the PRF family and a random oracle 
+ * (a function whose outputs are fixed completely at random).Pseudorandom function is the root of this family
  */
 package edu.biu.scapi.primitives.crypto.prf;
 
 import java.security.spec.AlgorithmParameterSpec;
-import java.security.spec.KeySpec;
+
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
 
 
 /** 
@@ -15,14 +20,14 @@ public interface PseudorandomFunction {
 	 * Initializes this prf with the secret key
 	 * @param secretKey - the secrete key
 	 *  */
-	public void init(KeySpec secretKey);
+	public void init(SecretKey secretKey);
 
 	/** 
 	 * Initializes this prf with the secret key and the auxiliary parameters
 	 * @param secretKey - secret key
 	 * @param params - algorithm parameters
 	 */
-	public void init(KeySpec secretKey, AlgorithmParameterSpec params);
+	public void init(SecretKey secretKey, AlgorithmParameterSpec params);
 
 	/** 
 	 * @return the parameter spec of this prf
@@ -33,7 +38,7 @@ public interface PseudorandomFunction {
 	/** 
 	 * @return the secret key
 	 */
-	public KeySpec getSecretKeySpec();
+	public SecretKey getSecretKey();
 
 
 	/** 
@@ -51,6 +56,41 @@ public interface PseudorandomFunction {
 	 * @param inOff - input offset in the inBytes array
 	 * @param outBytes - output bytes. The resulted bytes of compute.
 	 * @param outOff - output offset in the outBytes array to take the result from
+	 * @throws IllegalBlockSizeException 
 	 */
-	public void computetBlock(byte[] inBytes, int inOff, byte[] outBytes, int outOff);
+	public void computeBlock(byte[] inBytes, int inOff, byte[] outBytes, int outOff) throws IllegalBlockSizeException;
+	
+	/**
+	 * 
+	 * computetBlock
+	 * 
+	 * This function is necessary since some of the Prf's we implement may have variable input and output length.
+	 * If the implemented algorithm is a block cipher then the size of the input as well as the output is known in advence and 
+	 * the use may call the other computeBlock function where length is not require
+	 * @param inBytes- input bytes to compute
+	 * @param inLen - the length of the input array
+	 * @param inOff - input offset in the inBytes array
+	 * @param outBytes - output bytes. The resulted bytes of compute.
+	 * @param outOff - output offset in the outBytes array to take the result from
+	 * @param outLen - the length of the output array
+	 * @throws IllegalBlockSizeException 
+	 */
+	public void computeBlock(byte[] inBytes, int inOff, int inLen, byte[] outBytes, int outOff, int outLen) throws IllegalBlockSizeException;
+	
+	/**
+	 * 
+	 * computetBlock
+	 * 
+	 * This function is necessary since some of the Prf's we implement may have only variable input and fixed output length.
+	 * @param inBytes- input bytes to compute
+	 * @param inLen - the length of the input array
+	 * @param inOff - input offset in the inBytes array
+	 * @param outBytes - output bytes. The resulted bytes of compute.
+	 * @param outOff - output offset in the outBytes array to take the result from
+	 * @throws IllegalBlockSizeException 
+	 */
+
+	public void computeBlock(byte[] inBytes, int inOffset, int inLen, byte[] outBytes, int outOffset) throws IllegalBlockSizeException;;
+
+	
 }
--- Code/src/edu/biu/scapi/primitives/crypto/prf/PseudorandomPermutation.java
+++ Code/src/edu/biu/scapi/primitives/crypto/prf/PseudorandomPermutation.java
@@ -1,7 +1,11 @@
 /**
- * 
+ * Pseudorandom permutations are bijective pseudorandom functions that are efficiently invertible. 
+ * As such, they are of the pseudorandom function type and their input length always equals their output length. 
+ * In addition (and unlike general pseudorandom functions), they are efficiently invertible.
  */
 package edu.biu.scapi.primitives.crypto.prf;
+
+import javax.crypto.IllegalBlockSizeException;
 
 
 /** 
@@ -13,6 +17,17 @@ public interface PseudorandomPermutation extends PseudorandomFunction {
 	 * @param inOff - input offset in the inBytes array
 	 * @param outBytes - output bytes. The resulted bytes of invert.
 	 * @param outOff - output offset in the outBytes array to take the result from
+	 * @throws IllegalBlockSizeException 
 	 */
-	public void invertBlock(byte[] inBytes, int inOff, byte[] outBytes, int outOff);
+	public void invertBlock(byte[] inBytes, int inOff, byte[] outBytes, int outOff) throws IllegalBlockSizeException;
+	
+	/** 
+	 * @param inBytes - input bytes to invert
+	 * @param inOff - input offset in the inBytes array
+	 * @param outBytes - output bytes. The resulted bytes of invert.
+	 * @param outOff - output offset in the outBytes array to take the result from
+	 * @param len - the length of the input and the output.
+	 * @throws IllegalBlockSizeException 
+	 */
+	public void invertBlock(byte[] inBytes, int inOff, byte[] outBytes, int outOff, int len) throws IllegalBlockSizeException;
 }
