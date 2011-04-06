3a3e7b29e1a7991746479abe81194e2d47dc8ac0
Meital Levy <levymeit@gmail.com>
2011-04-06 06:52:06 +0000

    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@144 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/src/edu/biu/scapi/primitives/crypto/hash/CollisionResistantHash.java
+++ Code/src/edu/biu/scapi/primitives/crypto/hash/CollisionResistantHash.java
@@ -1,7 +1,8 @@
 /**
  * A cryptographic hash function is a deterministic procedure that takes an arbitrary block of data and returns a fixed-size bit string, 
- * the (cryptographic) hash value. There are two main levels of security that we will consider here: target collision resistance for which we have a (meaning that given x it is hard to find y such that H(y)=H(x)) and 
- * collision resistance (meaning that it is hard to find any x and y such that H(x)=H(y)). 
+ * the (cryptographic) hash value. There are two main levels of security that we will consider here: target collision resistance for which we have an interface for 
+ * in this package (meaning that given x it is hard to find y such that H(y)=H(x)) and 
+ * collision resistance - this interface (meaning that it is hard to find any x and y such that H(x)=H(y)). 
  * 
  */
 package edu.biu.scapi.primitives.crypto.hash;
