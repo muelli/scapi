c6b5aa1cff478b6042618b8be95cddd783a02701
Meital Levy <levymeit@gmail.com>
2011-05-05 08:12:57 +0000

    1. Bugs fixed 2. added some private functions
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@230 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/kdf/HKDF.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/kdf/HKDF.java
@@ -1,13 +1,18 @@
 /**
  * 
+ * This is a key derivation function that has a rigorous justification as to its security
  */
 package edu.biu.scapi.primitives.crypto.kdf;
+
+import java.util.logging.Level;
 
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+import edu.biu.scapi.generals.Logging;
 import edu.biu.scapi.primitives.crypto.prf.Hmac;
+import edu.biu.scapi.tools.Factories.PrfFactory;
 
 
 /** 
@@ -15,19 +20,20 @@ import edu.biu.scapi.primitives.crypto.prf.Hmac;
  */
 public class HKDF implements KeyDerivationFunction {
 	
-	private Hmac hmac;//the hmac to use. We assume that the hmac is initialized with a key and parameters
+	private Hmac hmac;
 
 	/*
 	 * We assume that the hmac is initialized with the required key.
 	 */
-	HKDF(Hmac hmac){
+	HKDF(String hmac){
 		
-		this.hmac = hmac;
+		this.hmac = (Hmac) PrfFactory.getInstance().getObject(hmac);
 	}
 
 	/**
 	 * generates a new key from the source key material key.
-	 * The pseudocode of thie function is as follows:
+	 * The pseudocode of this function is as follows:
+	 * 
 	 *   COMPUTE PRK = HMAC(XTS, SKM) [key=XTS, data=SKM]
 	 *   Let t be the smallest number so that t * |H|>L where |H| is the HMAC output length
 	 *   K(1) = HMAC(PRK,(CTXinfo,0)) [key=PRK, data=(CTXinfo,0)]
@@ -44,62 +50,134 @@ public class HKDF implements KeyDerivationFunction {
 		byte[] inBytes = key.getEncoded();                              //get the input key to work on
 		byte[] outBytes = new byte[outLen];                             //the output key
 		byte[] roundKey = new byte[hmacLength];							//PRK from the pseudocode
-		int rounds = (int) Math.floor((float)outLen/(float)hmacLength); //the smallest number so that  hmacLength * rounds > outLen
-		byte[] intermediateOutBytes = new byte[hmacLength];             //round result
-		int currentInBytesSize;											//the size of the CTXInfo and also the round;
+		byte[] intermediateOutBytes = new byte[hmacLength];             //round result K(i) in the pseudocode
 		
-		if(iv!=null)
-			currentInBytesSize = hmacLength + iv.length + 1;//the size of the CTXInfo and also the round;
-		else//no CTXInfo
-			currentInBytesSize = hmacLength + 1;//the size of the CTXInfo and also the round;
-		
-		byte[] currentInBytes = new byte[currentInBytesSize];
-		Integer round;
-		
-		//copy the CTXInfo - iv
-		if(iv!=null)
-			System.arraycopy(currentInBytes, 0, iv, 0, currentInBytesSize - 1);
 		
 		//first compute the new key. The new key is the result of computing the hmac function.
-		hmac.computetBlock(inBytes, 0, roundKey, 0);
+		try {
+			//roundKey is now K(0)
+			hmac.computeBlock(inBytes, 0, inBytes.length, roundKey, 0);
+		} catch (IllegalBlockSizeException e) {//should not happen since the roundKey is of the right size.
+			
+			Logging.getLogger().log(Level.WARNING, e.toString());
+		}
 		
 		//init the hmac with the new key. From now on this is the key for all the rounds.
 		hmac.init(new SecretKeySpec(roundKey, "HKDF"));
 		
-		//copy the roundKey which is also part of the input to the nexxt iteration
-		System.arraycopy(roundKey, 0, intermediateOutBytes, 0, hmacLength);
+		//calculate the first round
+		//K(1) = HMAC(PRK,(CTXinfo,0)) [key=PRK, data=(CTXinfo,0)]
+		firstRound(outBytes, iv, intermediateOutBytes, hmacLength);
+		
+		//calculate the next rounds
+		//FOR i = 2 TO t
+		//K(i) = HMAC(PRK,(K(i-1),CTXinfo,i)) [key=PRK, data=(K(i-1),CTXinfo,i)]
+		nextRounds(outLen, iv, hmacLength, outBytes, 
+				intermediateOutBytes);
+		
+		
+		return new SecretKeySpec(outBytes, "HKDF");
+	}
+
+	/**
+	 * nextRounds : does the following part of the pseudo code:
+	 * FOR i = 2 TO t
+	 * K(i) = HMAC(PRK,(K(i-1),CTXinfo,i)) [key=PRK, data=(K(i-1),CTXinfo,i)]
+	 * @param outLen
+	 * @param iv - the iv : ctxInfo
+	 * @param hmacLength - the size of the output of the hmac.
+	 * @param outBytes - the result of the overall computation
+	 * @param intermediateOutBytes - round result K(i) in the pseudocode
+	 */
+	private void nextRounds(int outLen, byte[] iv, int hmacLength,
+			byte[] outBytes, byte[] intermediateOutBytes) {
+		
+		int rounds = (int) Math.ceil((float)outLen/(float)hmacLength); //the smallest number so that  hmacLength * rounds >= outLen
+		
+		int currentInBytesSize;	//the size of the CTXInfo and also the round;
+		
+		if(iv!=null)
+			currentInBytesSize = hmacLength + iv.length + 1;//the size of the CTXInfo and also the round;
+		else//no CTXInfo
+			currentInBytesSize = hmacLength + 1;//the size without the CTXInfo and also the round;
+		
+		//the result of the current computation
+		byte[] currentInBytes = new byte[currentInBytesSize];
+		
+		
+		Integer roundIndex;
+		//for rounds 2 to t 
+		if(iv!=null)
+			//in case we have an iv. put it (ctxInfo after the K from the previous round at position hmacLength.
+			System.arraycopy(iv, 0, currentInBytes, hmacLength , iv.length);
+				
+		for(int i=2;i<=rounds; i++){
 			
-		for(int i=0;i<rounds; i++){
-			
-			round = new Integer(i);//create the round integer for the date
+			roundIndex = new Integer(i-1);//create the round integer for the data
 			
 			//copy the output of the last results
 			System.arraycopy(intermediateOutBytes, 0, currentInBytes, 0, hmacLength);
 			
-			//copy the round integer to the date array 
-			System.arraycopy(round.byteValue(), 0,currentInBytes , currentInBytesSize -1, 1);
+			//copy the round integer to the data array 
+			System.arraycopy(roundIndex.byteValue(), 0,currentInBytes , currentInBytesSize -1, 1);
 			
 			
 			//operate the hmac to get the round output 
 			try {
-				hmac.computetBlock(currentInBytes, 0, currentInBytes.length, intermediateOutBytes, 0, hmacLength);
+				hmac.computeBlock(currentInBytes, 0, currentInBytes.length, intermediateOutBytes, 0);
 			} catch (IllegalBlockSizeException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				
+				Logging.getLogger().log(Level.WARNING, e.toString());
 			}
 			
-			if(i==rounds - 1){//We fill the rest of the array with a portion of the last result.
+			if(i==rounds){//We fill the rest of the array with a portion of the last result.
 				
 				//copy the results to the output array
-				System.arraycopy(intermediateOutBytes, 0,outBytes , 0, outLen - hmacLength*i);
+				System.arraycopy(intermediateOutBytes, 0,outBytes , hmacLength*(i-1), outLen - hmacLength*i);
 			}
 			else{
 				//copy the results to the output array
-				System.arraycopy(intermediateOutBytes, 0,outBytes , 0, hmacLength*i);
+				System.arraycopy(intermediateOutBytes, 0,outBytes , hmacLength*(i-1), hmacLength);
 			}				
 		}
+	}
+
+	/**
+	 * firstRound
+	 * @param iv - ctxInfo
+	 * @param intermediateOutBytes - round result K(1) in the pseudocode
+	 * @param hmacLength - the size of the output of the hmac.
+	 * @param outBytes - the result of the overall computation
+	 */
+	private void firstRound(byte [] outBytes, byte[] iv, byte[] intermediateOutBytes, int hmacLength) {
+		Integer zero;
+		//round 1
+		byte[] firstRoundInput;//data for the creating K(1)
+		if(iv!=null)
+			firstRoundInput = new  byte[iv.length + 1];
+		else
+			firstRoundInput = new  byte[1];
 		
-		return new SecretKeySpec(outBytes, "HKDF");
+		//copy the CTXInfo - iv
+		if(iv!=null)
+			System.arraycopy(iv, 0, firstRoundInput,0 , iv.length);
+		
+		zero = new Integer(0);//create the round integer for the date
+		
+		//copy the integer with zero to the data array 
+		System.arraycopy(zero.byteValue(), 0,firstRoundInput , firstRoundInput.length -1, 1);
+		
+			
+		//first compute the new key. The new key is the result of computing the hmac function.
+		try {
+			//calculate K(1) and put it in intermediateOutBytes.
+			hmac.computeBlock(firstRoundInput, 0, firstRoundInput.length, intermediateOutBytes, 0);
+		} catch (IllegalBlockSizeException e) {	
+		
+		}
+		
+		//copy the results to the output array
+		System.arraycopy(intermediateOutBytes, 0,outBytes , 0, hmacLength);
 	}
 
 	public SecretKey generateKey(SecretKey key, int outLen) {
