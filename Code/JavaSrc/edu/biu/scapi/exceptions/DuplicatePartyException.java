6de4a08ac7d9c49ad17ac17116760ee023d7018b
Meital Levy <levymeit@gmail.com>
2011-05-08 08:52:36 +0000

    Initial version. Includes scapi exceptions.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@253 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/exceptions/DuplicatePartyException.java
@@ -0,0 +1,29 @@
+/**
+ * Project: scapi.
+ * Package: edu.biu.scapi.exceptions.
+ * File: DuplicatePartyException.java.
+ * Creation date Mar 7, 2011
+ * Create by LabTest
+ *
+ *
+ * This file TODO
+ */
+package edu.biu.scapi.exceptions;
+
+/**
+ * @author LabTest
+ *
+ */
+public class DuplicatePartyException extends Exception {
+
+	/**
+	 * 
+	 */
+	public DuplicatePartyException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public DuplicatePartyException(String message){
+		super(message);
+	}
+}
