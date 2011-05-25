1b403adcbbd2c393dcac4fca40d4a78d3b041445
Meital Levy <levymeit@gmail.com>
2011-05-25 09:22:19 +0000

    Comment correction.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@312 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/prf/PrpVaryingIOLength.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/prf/PrpVaryingIOLength.java
@@ -1,5 +1,5 @@
 /**
- * A pseudorandom permutation with varying input/output lengths does not have pre-defined input /output lengths. 
+ * A pseudorandom permutation with varying input/output lengths does not have predefined input /output lengths. 
  * The input and output length (that must be equal) may be different for each function call. 
  * The length of the input/output is determined upon user request. 
  * The interface PrpVaryingIOLength, groups and provides type safety for every PRP with varying input/output length. 
