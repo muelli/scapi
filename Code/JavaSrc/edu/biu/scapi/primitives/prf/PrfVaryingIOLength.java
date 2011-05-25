828949ec08b76a6fabfc3c9d1c7eee75597f7c8d
Meital Levy <levymeit@gmail.com>
2011-05-25 09:06:21 +0000

    Comment correction.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@309 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/prf/PrfVaryingIOLength.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/prf/PrfVaryingIOLength.java
@@ -1,5 +1,5 @@
 /**
- * A pseudorandom function with varying input/output lengths does not have pre-defined input and output lengths. 
+ * A pseudorandom function with varying input/output lengths does not have predefined input and output lengths. 
  * The input and output length may be different for each compute function call. 
  * The length of the input as well as the output is determined upon user request.
  * The interface PrfVaryingIOLength, groups and provides type safety for every PRF with varying input and output length
