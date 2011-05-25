7934d3c640763ac30dcc251525096a377969cc28
Meital Levy <levymeit@gmail.com>
2011-05-25 07:57:29 +0000

    Comment correction.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@294 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/generals/LoggerExtendedLevel.java
+++ Code/JavaSrc/edu/biu/scapi/generals/LoggerExtendedLevel.java
@@ -3,10 +3,12 @@
  * Package: edu.biu.scapi.generals.
  * File: LoggerExtendedLevel.java.
  * Creation date Mar 13, 2011
- * Create by LabTest
+ * Created by LabTest
  *
  *
- * This file TODO
+ * This class extends the Level class of the logger. The aim is to generate additional levels that will
+ * suite the scapi project. For example, we create a TIMING level who's aim is to log only timing records
+ * and thus must have a higher level than severe. 
  */
 package edu.biu.scapi.generals;
 
@@ -15,9 +17,7 @@ import java.util.logging.Level;
 /**
  * @author LabTest
  * 
- * This class extends the Level class of the logger. The aim is to generate additional levels that will
- * suite the scapi project. For example, we create a TIMING level who's aim is to log only timing records
- * and thus must have a higher level than severe. 
+ * 
  *
  */
 public class LoggerExtendedLevel extends Level {
