c9e46f06a423ecb176a53b83dfaf7a53625174ce
Meital Levy <levymeit@gmail.com>
2011-05-25 09:34:12 +0000

    Changing the class to be final and changing comments to follow the guidelines of javadoc.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@315 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/prf/bc/BcHMAC.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/prf/bc/BcHMAC.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * An adapter class that wrapps the Hmac of bouncy castle.
  */
 package edu.biu.scapi.primitives.prf.bc;
 
@@ -23,19 +23,20 @@ import edu.biu.scapi.tools.Translation.BCParametersTranslator;
 	/** 
 	 * @author LabTest
 	 */
-public class BcHMAC implements  Hmac {
+public final class BcHMAC implements  Hmac {
 	
 	private HMac hMac;//The underlying wrapped hmac of BC.
 	private AlgorithmParameterSpec params = null;
 	private SecretKey secretKey = null;
 
 	/** 
-	 * @param hash - the hash function to translate into digest of bc hmac
+	 * A constructor that can be called from the factory
+	 * @param hashName - the hash function to translate into digest of bc hmac
 	 */
-	public BcHMAC(String hash) {
+	public BcHMAC(String hashName) {
 		
 		Digest digest = null;
-		digest = BCFactory.getInstance().getDigest(hash);
+		digest = BCFactory.getInstance().getDigest(hashName);
 		
 		
 		//create the Hmac of BC
@@ -45,75 +46,19 @@ public class BcHMAC implements  Hmac {
 	}
 
 	/** 
-	 * @param secretKey - secret key 
-	 * @param params - algorithm parameter
+	 * Initializes this hmac with the secret key and the auxiliary parameters
+	 * @param secretKey secret key 
+	 * @param params algorithm parameter
 	 */
 	public void init(SecretKey secretKey, AlgorithmParameterSpec params) {
 		
 		//no auxiliary parameters for HMAC. Pass the key
 		init(secretKey);
 	}
-
-	/** 
-	 * @param inBytes - input bytes to compute
-	 * @param inOff - input offset in the inBytes array
-	 * @param outBytes - output bytes. The resulted bytes of compute.
-	 * @param outOff - output offset in the outBytes array to take the result from
-	 * @throws IllegalBlockSizeException 
-	 */
-	public void computeBlock(byte[] inBytes, int inOff, byte[] outBytes, int outOff) throws IllegalBlockSizeException{
-		
-		throw new IllegalBlockSizeException("Size of input is not specified");
-	}
 	
-	/**
-	 * 
-	 * computetBlock : 
-	 * 
-	 * 
-	 * @param inBytes- input bytes to compute
-	 * @param inLen - the length of the input array
-	 * @param inOff - input offset in the inBytes array
-	 * @param outBytes - output bytes. The resulted bytes of compute.
-	 * @param outOff - output offset in the outBytes array to take the result from
-	 * @param outLen - the length of the output array
-	 * @throws IllegalBlockSizeException 
-	 */
-	public void computeBlock(byte[] inBytes, int inOff, int inLen, byte[] outBytes, int outOff, int outLen) throws IllegalBlockSizeException{
-		
-		//make sure the output size is correct
-		if(outLen==hMac.getMacSize())
-			computeBlock(inBytes, inOff, inLen, outBytes, outOff);
-		else
-			throw new IllegalBlockSizeException("Output size is incorrect");
-	}
-	
-	/**
-	 * 
-	 * computetBlock : 
-	 * 
-	 * 
-	 * @param inBytes- input bytes to compute
-	 * @param inLen - the length of the input array
-	 * @param inOff - input offset in the inBytes array
-	 * @param outBytes - output bytes. The resulted bytes of compute.
-	 * @param outOff - output offset in the outBytes array to take the result from
-	 */
-
-	public void computeBlock(byte[] inBytes, int inOffset, int inLen,
-			byte[] outBytes, int outOffset) throws IllegalBlockSizeException {
-		
-		//pass the input bytes to update
-		hMac.update(inBytes, inOffset, inLen);
-		
-		//get the output results through doFinal
-		hMac.doFinal(outBytes, outOffset);
-	}
-	
-
 	/** 
 	 * Initializes the bc underlying hmac
-	 * @param secretKey - the secret key to convert to bc key parameter
+	 * @param secretKey the secret key to convert to bc key parameter
 	 */
 	public void init(SecretKey secretKey) {
 		
@@ -128,9 +73,75 @@ public class BcHMAC implements  Hmac {
 		hMac.init(bcParams);
 		
 	}
+	
+	
 
 	/** 
-	 * @return - the name from bc hmac
+	 * Since the input is not fixed, it must be supplied. This function should not be called. 
+	 * If the user still calls this function, throw an exception.
+	 * @param inBytes input bytes to compute
+	 * @param inOff input offset in the inBytes array
+	 * @param outBytes output bytes. The resulted bytes of compute
+	 * @param outOff output offset in the outBytes array to take the result from
+	 * @throws IllegalBlockSizeException 
+	 */
+	public void computeBlock(byte[] inBytes, int inOff, byte[] outBytes, int outOff) throws IllegalBlockSizeException{
+		
+		throw new IllegalBlockSizeException("Size of input is not specified");
+	}
+	
+	/**
+	 * 
+	 * Since both output variable is fixed this function should not normally be called. 
+	 * If the user still wants to use this function, the specified argument outLen should be the same as 
+	 * the result of getMacSize from BC, otherwise, throw an exception. 
+	 * 
+	 * @param inBytes input bytes to compute
+	 * @param inLen the length of the input array
+	 * @param inOff input offset in the inBytes array
+	 * @param outBytes output bytes. The resulted bytes of compute
+	 * @param outOff output offset in the outBytes array to take the result from
+	 * @param outLen the length of the output array
+	 * @throws IllegalBlockSizeException 
+	 */
+	public void computeBlock(byte[] inBytes, int inOff, int inLen, byte[] outBytes, int outOff, int outLen) throws IllegalBlockSizeException{
+		
+		//make sure the output size is correct
+		if(outLen==hMac.getMacSize())
+			computeBlock(inBytes, inOff, inLen, outBytes, outOff);
+		else
+			throw new IllegalBlockSizeException("Output size is incorrect");
+	}
+	
+	/**
+	 * 
+	 * Computes the function using the secret key. The user supplies the input byte array and the offset from 
+	 * which to take the data from. Also since the input is not fixed the input is supplied as well. 
+	 * The user also supplies the output byte array as well as the offset. 
+	 * The computeBlock function will put the output starting at the offset. 
+	 * 
+	 * @param inBytes input bytes to compute
+	 * @param inLen the length of the input array
+	 * @param inOffset input offset in the inBytes array
+	 * @param outBytes output bytes. The resulted bytes of compute
+	 * @param outOffset output offset in the outBytes array to take the result from
+	 */
+
+	public void computeBlock(byte[] inBytes, int inOffset, int inLen,
+			byte[] outBytes, int outOffset) throws IllegalBlockSizeException {
+		
+		//pass the input bytes to update
+		hMac.update(inBytes, inOffset, inLen);
+		
+		//get the output results through doFinal
+		hMac.doFinal(outBytes, outOffset);
+	}
+	
+
+	
+
+	/** 
+	 * @return the name from BC hmac
 	 */
 	public String getAlgorithmName() {
 		
@@ -138,7 +149,7 @@ public class BcHMAC implements  Hmac {
 	}
 
 	/**
-	 * @return - the block size of the BC hmac
+	 * @return the block size of the BC hmac
 	 */
 	public int getBlockSize() {
 		
@@ -155,7 +166,7 @@ public class BcHMAC implements  Hmac {
 
 
 	/**
-	 * @return - the secret key
+	 * @return the secret key
 	 */
 	public SecretKey getSecretKey() {
 		return secretKey;
