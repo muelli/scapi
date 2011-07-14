a9bf2d6477a12d993518d9f9638026eb531d0c3d
Meital Levy <levymeit@gmail.com>
2011-07-14 11:46:13 +0000

    Adding init and isInitialized function.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@370 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/kdf/KeyDerivationFunction.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/kdf/KeyDerivationFunction.java
@@ -5,6 +5,8 @@
  */
 package edu.biu.scapi.primitives.kdf;
 
+import java.security.spec.AlgorithmParameterSpec;
+
 import javax.crypto.SecretKey;
 
 /** 
@@ -20,4 +22,23 @@ public interface KeyDerivationFunction {
 	public SecretKey generateKey(SecretKey key, int outLen,  byte[] iv);
 	public SecretKey generateKey(SecretKey key, int outLen);
 	public void generateKey(byte[] inKey, int inOff, int inLen, byte[] outKey, int outOff, int outLen);
+	
+	/**
+	 * Initializes this krf with the secret key.
+	 * @param secretKey the secrete key
+	 *  */
+	public void init(SecretKey secretKey);
+
+	/** 
+	 * Initializes this krf with the secret key and the auxiliary parameters.
+	 * @param secretKey secret key
+	 * @param params algorithm parameters
+	 */
+	public void init(SecretKey secretKey, AlgorithmParameterSpec params);
+	
+	/**
+	 * 
+	 * @return true if the object was initialized by calling the function init.
+	 */
+	public boolean isInitialized();
 }
