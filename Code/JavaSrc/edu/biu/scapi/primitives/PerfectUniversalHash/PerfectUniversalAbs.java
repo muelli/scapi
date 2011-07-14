5bdd19ed0030f6c8b9ae236b58bb9a575722f0e3
Meital Levy <levymeit@gmail.com>
2011-07-14 11:56:04 +0000

    Adding init and isInitialized function.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@374 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/PerfectUniversalHash/PerfectUniversalAbs.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/PerfectUniversalHash/PerfectUniversalAbs.java
@@ -5,11 +5,15 @@ package edu.biu.scapi.primitives.PerfectUniversalHash;
 
 import java.security.spec.AlgorithmParameterSpec;
 
+import javax.crypto.SecretKey;
+
 /** 
   * @author LabTest
  */
 public abstract class PerfectUniversalAbs implements PerfectUniversalHash {
-	protected AlgorithmParameterSpec params;
+	protected AlgorithmParameterSpec params = null;
+	protected SecretKey secretKey = null;
+	
 	protected boolean isInitialized = true;//most target collision resistant hash functions do not need to call init
 										   //if a certain hash does need to pass some parameters in init, it must set this
 										   //flag to false in the constructor and to true in the init function.
@@ -18,11 +22,24 @@ public abstract class PerfectUniversalAbs implements PerfectUniversalHash {
 	 * Initialize this perfect universal hash with the auxiliary parameters 
 	 * @param params
 	 */
-	public void init(AlgorithmParameterSpec params) {
+	public void init(SecretKey secretKey) {
 
 		isInitialized = true;
-		this.params = params;
+		this.secretKey = secretKey;
 	}
+	
+	/** 
+	 * Initializes this PerfectUniversalHash with the secret key and the auxiliary parameters.
+	 * @param secretKey secret key
+	 * @param params algorithm parameters
+	 */
+	public void init(SecretKey secretKey, AlgorithmParameterSpec params){
+		
+		isInitialized = true;
+		this.params = params;
+		this.secretKey = secretKey;
+	}
+
 
 	/**
 	 * 
@@ -36,8 +53,3 @@ public abstract class PerfectUniversalAbs implements PerfectUniversalHash {
 	 * @return the parameter spec of this perfect universal hash
 	 */
 	public AlgorithmParameterSpec getParams() {
-
-		return params;
-	}
-	
-}
