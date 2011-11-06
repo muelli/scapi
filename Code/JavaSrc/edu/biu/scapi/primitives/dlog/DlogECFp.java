bacaecacf7798729cd9ef726d69132a0cc14e865
Yael Ejgenberg <yaelnegje@yahoo.com>
2011-11-06 11:49:14 +0000

    More comment's changes
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@545 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/DlogECFp.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/DlogECFp.java
@@ -2,7 +2,7 @@ package edu.biu.scapi.primitives.dlog;
 
 /**
  * Marker interface. Every class that implements it is signed as elliptic curve over F[p]
- * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moryia Farbstein)
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
 public interface DlogECFp extends DlogEllipticCurve{
