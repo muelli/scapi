25a3948abd8fd6999be8d994e51f1a74e855e991
Moriya Farbstein <moriyaw@gmail.com>
2011-11-06 10:58:18 +0000

    add getX and getY functions
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@526 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/ECElement.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/ECElement.java
@@ -1,11 +1,14 @@
 package edu.biu.scapi.primitives.dlog;
 
+import java.math.BigInteger;
+
 
 /**
  * Marker interface. Every class that implements it is signed as elliptic curve point
- * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moryia Farbstein)
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
 public interface ECElement extends GroupElement{
-
+	public BigInteger getX();
+	public BigInteger getY();
 }
