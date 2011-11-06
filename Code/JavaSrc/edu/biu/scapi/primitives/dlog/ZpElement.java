b6d3d3c85d1ff0b5ab3e09e0a14c12d0d1ec106e
Moriya Farbstein <moriyaw@gmail.com>
2011-11-06 10:58:48 +0000

    add getElementValue function
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@527 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/ZpElement.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/ZpElement.java
@@ -1,10 +1,12 @@
 package edu.biu.scapi.primitives.dlog;
 
+import java.math.BigInteger;
+
 /**
  * Marker interface. Every class that implements it is signed as Zp* element
- * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moryia Farbstein)
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
 public interface ZpElement extends GroupElement{
-
+	public BigInteger getElementValue();
 }
