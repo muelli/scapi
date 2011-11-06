5bf96aff80d0f34968212c60462e01124c768b51
Moriya Farbstein <moriyaw@gmail.com>
2011-11-06 11:05:03 +0000

    add getX and getY functions
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@530 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/bc/ECPointBc.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/bc/ECPointBc.java
@@ -16,10 +16,18 @@ public abstract class ECPointBc implements ECElement{
 
 	protected ECPoint point = null;
 	
-	public ECPoint getPoint(){
+	ECPoint getPoint(){
 		return point;
 	}
 
+	public BigInteger getX(){
+		return point.getX().toBigInteger();
+	}
+	
+	public BigInteger getY(){
+		return point.getY().toBigInteger();
+	}
+	
 	abstract boolean checkValidity(BigInteger bigInteger, BigInteger bigInteger2,
 			ECGroupParams groupParams);
 	
