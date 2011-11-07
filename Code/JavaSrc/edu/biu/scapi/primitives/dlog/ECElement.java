d661ff519c23ab5c987a16a7a741c515538451a2
Yael Ejgenberg <yaelnegje@yahoo.com>
2011-11-07 13:39:40 +0000

    More comment's changes
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@570 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/ECElement.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/ECElement.java
@@ -4,11 +4,20 @@ import java.math.BigInteger;
 
 
 /**
- * Marker interface. Every class that implements it is signed as elliptic curve point
+ * Marker interface. Every class that implements it, is signed as an elliptic curve point
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
 public interface ECElement extends GroupElement{
+	/**
+	 * This function returns the x coordinate of the (x,y) point which is an element of a given elliptic curve.
+	 * @return x coordinate of (x,y) point
+	 */
 	public BigInteger getX();
+	
+	/**
+	 * This function returns the y coordinate of the (x,y) point which is an element of a given elliptic curve.
+	 * @return y coordinate of (x,y) point
+	 */
 	public BigInteger getY();
 }
