62c25d52c02b3a7c0e9ac085eca0ed1ad98c55cb
Yael Ejgenberg <yaelnegje@yahoo.com>
2011-11-09 13:45:43 +0000

    More comment's changes
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@604 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/GroupElement.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/GroupElement.java
@@ -1,7 +1,9 @@
 package edu.biu.scapi.primitives.dlog;
 
 /**
- * GroupElement family, which holds the concrete elements for the concrete Dlog groups.
+ * This is the main interface of the Group element hierarchy.<p> 
+ * We can refer to a group element as a general term OR we can relate to the fact that an element of an elliptic curve
+ * is a point and an element of a Zp group is a number between 0 and p-1.
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/ZpElement.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/ZpElement.java
@@ -3,10 +3,14 @@ package edu.biu.scapi.primitives.dlog;
 import java.math.BigInteger;
 
 /**
- * Marker interface. Every class that implements it is signed as Zp* element
+ * This is a marker interface. Every class that implements it is signed as Zp* element. 
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
 public interface ZpElement extends GroupElement{
+	/**
+	 * This function returns the actual "integer" value of this element; which is an element of a given Dlog over Zp*.
+	 * @return integer value of this Zp element.
+	 */
 	public BigInteger getElementValue();
 }
