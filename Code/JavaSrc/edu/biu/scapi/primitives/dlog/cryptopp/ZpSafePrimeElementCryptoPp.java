f6079b8676c47024db0ec40b2b9104be9fcbc28d
Yael Ejgenberg <yaelnegje@yahoo.com>
2011-11-13 10:50:04 +0000

    Enclose internal if with parenthesis. In general even if we have an if clause that if true then the code is only one line we do not "save" the parenthesis but we enclose the one line in parenthesis.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@606 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/cryptopp/ZpSafePrimeElementCryptoPp.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/cryptopp/ZpSafePrimeElementCryptoPp.java
@@ -32,10 +32,11 @@ public class ZpSafePrimeElementCryptoPp implements ZpElement{
 		if(bCheckMembership){
 			BigInteger q = p.subtract(BigInteger.ONE).divide(new BigInteger("2"));
 			//if the element is in the expected range, set it. else, throw exception
-			if ((x.compareTo(BigInteger.ZERO)>0) && (x.compareTo(p.subtract(BigInteger.ONE))<=0))
+			if ((x.compareTo(BigInteger.ZERO)>0) && (x.compareTo(p.subtract(BigInteger.ONE))<=0)){
 				if ((x.modPow(q, p)).compareTo(BigInteger.ONE)==0){
 					pointerToElement = getPointerToElement(x.toByteArray());
 				}
+			}
 			else throw new IllegalArgumentException("Cannot create Zp element. Requested value " + x + " is not in the range of this group.");
 		} else {
 			pointerToElement = getPointerToElement(x.toByteArray());
