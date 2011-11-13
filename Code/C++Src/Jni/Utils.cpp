d77d08d8082152ddf560c4630418210aa0d5c4a5
Yael Ejgenberg <yaelnegje@yahoo.com>
2011-11-13 11:11:18 +0000

    No ; after include.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@612 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/C++Src/Jni/Utils.cpp
+++ Code/C++Src/Jni/Utils.cpp
@@ -1,8 +1,8 @@
 #include "stdafx.h"
 #include "Utils.h"
-#include "Integer.h";
-#include <iostream>;
-#include <queue.h>;
+#include "Integer.h"
+#include <iostream>
+#include <queue.h>
 
 using namespace std;
 
