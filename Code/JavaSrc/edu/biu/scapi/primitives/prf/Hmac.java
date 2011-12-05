0334dfd1f25cefb51eac2a2af7e925e395fb55e4
Moriya Farbstein <moriyaw@gmail.com>
2011-12-05 09:19:27 +0000

    fixed comments to match javadoc guideline
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@634 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/prf/Hmac.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/prf/Hmac.java
@@ -1,10 +1,10 @@
-/**
- * Hmac has varying input length and thus implements the interface PrfVaryingInputLength.
- */
 package edu.biu.scapi.primitives.prf;
 
 /** 
- * @author LabTest
+ * Marker interface. Every class that implements it is signed as Hmac.
+ * Hmac has varying input length and thus implements the interface PrfVaryingInputLength.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
  */
 public interface Hmac extends PrfVaryingInputLength {
 }
