8d36c77140a4a9a321c8f9d29b3ddd8e6da8816e
Moriya Farbstein <moriyaw@gmail.com>
2011-12-05 09:03:18 +0000

    1.fixed comments to match javadoc guideline 2. deleted class members algorithmParameterSpec, secretKey and isInitialized since the are saved in the underlying prf and there is no need to save the m in this class too
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@628 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/prf/PrpFromPrfVarying.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/prf/PrpFromPrfVarying.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package edu.biu.scapi.primitives.prf;
 
 import java.security.spec.AlgorithmParameterSpec;
@@ -12,68 +9,55 @@ import javax.crypto.SecretKey;
 import edu.biu.scapi.exceptions.UnInitializedException;
 
 /** 
- * @author LabTest
+ * This class implements some common functionality of PrpVaryingIOLength by having an instance of prfVaryingIOLength.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
  */
 public abstract class PrpFromPrfVarying implements PrpVaryingIOLength {
 	
-	protected PrfVaryingIOLength prfVaryingIOLength;
-	protected AlgorithmParameterSpec params = null;
-	protected SecretKey secretKey = null;
-	protected boolean isInitialized = false;//until isInitialized() is called set to false.
+	protected PrfVaryingIOLength prfVaryingIOLength; // the underlying prf
 	
+	/**
+	 * Initializes this PrpFromPrfVarying with secret key
+	 * @param secretKey the secret key
+	 */
 	public void init(SecretKey secretKey) {
-
+		//initializes the underlying prf with the given secret key
 		prfVaryingIOLength.init(secretKey);
 		
 	}
 
 	/**
-	 * Initializes this PrpFromPrfFixed with the secret key
-	 * @param secretKey the secrete key
+	 * Initializes this PrpFromPrfVarying with the secret key and the auxiliary parameters
+	 * @param secretKey the secret key
 	 * @param params the auxiliary parameters
 	 * @throws InvalidParameterSpecException 
 	 */
 	
 	public void init(SecretKey secretKey, AlgorithmParameterSpec params) throws InvalidParameterSpecException {
-
-		prfVaryingIOLength.init(secretKey, params);
-		
+		//initializes the underlying prf with the given secret key and params
+		prfVaryingIOLength.init(secretKey, params);	
 	}
 	
-	/**
-	 * 
-	 * @return true if the object was initialized by calling the function init.
-	 */
 	public boolean isInitialized(){
+		// call the underlying prf isInitialized function and return the result
 		return prfVaryingIOLength.isInitialized();
 	}
 	
-	/** 
-	 * @return the parameters spec
-	 * @throws UnInitializedException 
-	 */
 	public AlgorithmParameterSpec getParams() throws UnInitializedException {
+		// return the params of the underlying prf
 		return prfVaryingIOLength.getParams();
 	}
 
-
-
-	/**
-	 * @return the secret key
-	 * @throws UnInitializedException 
-	 */
 	public SecretKey getSecretKey() throws UnInitializedException {
+		// return the secret key of the underlying prf
 		return prfVaryingIOLength.getSecretKey();
 	}
 
-
-
 	/** 
-	 * Since both Input and output variables are varying this function should not be called. Throw an exception.
-	 * @param inBytes input bytes to compute
-	 * @param inOff input offset in the inBytes array
-	 * @param outBytes output bytes. The resulted bytes of compute
-	 * @param outOff output offset in the outBytes array to take the result from
+	 * This function is suitable for block ciphers where the input/output length is known in advance.
+	 * In this case, both input and output variables are varying so this function should not be called. Throws an exception.
+	 * 
 	 * @throws IllegalBlockSizeException 
 	 * @throws UnInitializedException 
 	 */
@@ -83,40 +67,52 @@ public abstract class PrpFromPrfVarying implements PrpVaryingIOLength {
 		if(!isInitialized()){
 			throw new UnInitializedException();
 		}
-		throw new IllegalBlockSizeException("Input and output sizes are not specified");
+		throw new IllegalBlockSizeException("to use this prp, call the computeBlock function which specify the block size length");
 		
 	}
 
-
-
 	/** 
-	 * Since both Input and output variables are varying this function should not be called. Throw an exception.
+	 * Computes the function using the secret key. <p>
+	 * 
+	 * This function is provided in the interface especially for the sub-family PrfVaryingIOLength, which may have variable input and output length.
+	 * Since this is a prp, both input and output variables should be equal and there is no need to send them both, so this function should not be called. 
+	 * If the user still wants to use this function, the specified arguments <code>inLen<code> and <code>outLen<code> should be the same, otherwise, throws an exception.
+	 * 
 	 * @param inBytes input bytes to compute
 	 * @param inOff input offset in the inBytes array
+	 * @param inLen the length of the input array
 	 * @param outBytes output bytes. The resulted bytes of compute
-	 * @param outOff output offset in the outBytes array to take the result from
+	 * @param outOff output offset in the outBytes array to put the result from
+	 * @param outLen the length of the output array
 	 * @throws IllegalBlockSizeException 
 	 * @throws UnInitializedException 
 	 */
 	public void computeBlock(byte[] inBytes, int inOff, int inLen,
-			byte[] outBytes, int outOff)
+			byte[] outBytes, int outOff, int outLen)
 			throws IllegalBlockSizeException, UnInitializedException {
 		if(!isInitialized()){
 			throw new UnInitializedException();
 		}
-		throw new IllegalBlockSizeException("Input size is not specified");
+		// checks that the offsets and lengths are correct 
+		if ((inOff > inBytes.length) || (inOff+inLen > inBytes.length)){
+			throw new ArrayIndexOutOfBoundsException("wrong offset for the given input buffer");
+		}
+		if ((outOff > outBytes.length) || (outOff+outLen > outBytes.length)){
+			throw new ArrayIndexOutOfBoundsException("wrong offset for the given output buffer");
+		}
+		
+		//if input and output lengths are equal, call the computeBlock which takes just one length argument
+		if (inLen == outLen){
+			computeBlock(inBytes, inOff, inLen, outBytes, outOff);
+		}
+		else throw new IllegalBlockSizeException("input and output lengths should be equal");
 		
 	}
 
 	/**
-	 * since both Input and output variables are varying this function should not normally be called. 
-	 * If the user still wants to use this function, the specified argument <code>len<code> should be the same as 
-	 * the result of getBlockSize, otherwise, throw an exception. 
-	 * @param inBytes input bytes to invert
-	 * @param inOff input offset in the inBytes array
-	 * @param outBytes output bytes. The resulted bytes of invert
-	 * @param outOff output offset in the outBytes array to take the result from
-	 * @param len the length of the input and the output.
+	 * This function is suitable for block ciphers where the input/output length is known in advance.
+	 * In this case, both input and output variables are varying so this function should not be called. Throws an exception.
+	 * 
 	 * @throws IllegalBlockSizeException 
 	 * @throws UnInitializedException 
 	 */
@@ -125,7 +121,7 @@ public abstract class PrpFromPrfVarying implements PrpVaryingIOLength {
 		if(!isInitialized()){
 			throw new UnInitializedException();
 		}
-		throw new IllegalBlockSizeException("Wrong size");
+		throw new IllegalBlockSizeException("to use this prp, call the invertBlock function which specify the block size length");
 		
 	}
 	
