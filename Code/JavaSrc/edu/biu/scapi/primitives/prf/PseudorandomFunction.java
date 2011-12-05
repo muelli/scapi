d9695c3c732018b1d09cbe6a3dc5e5b98b91aa1e
Moriya Farbstein <moriyaw@gmail.com>
2011-12-05 08:58:05 +0000

    fixed comments to match javadoc guideline
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@627 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/prf/PseudorandomFunction.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/prf/PseudorandomFunction.java
@@ -1,9 +1,3 @@
-/**
- * Pseudorandom function definition: In cryptography, a pseudorandom function family, abbreviated PRF, 
- * is a collection of efficiently-computable functions which emulate a random oracle in the following way: 
- * no efficient algorithm can distinguish (with significant advantage) between a function chosen randomly from the PRF family and a random oracle 
- * (a function whose outputs are fixed completely at random).Pseudorandom function is the root of this family
- */
 package edu.biu.scapi.primitives.prf;
 
 import java.security.spec.AlgorithmParameterSpec;
@@ -16,7 +10,14 @@ import edu.biu.scapi.exceptions.UnInitializedException;
 
 
 /** 
- * @author LabTest
+ * General interface for pseudorandom function. Every class in this family should implement this interface. <p>
+ * 
+ * In cryptography, a pseudorandom function family, abbreviated PRF, 
+ * is a collection of efficiently-computable functions which emulate a random oracle in the following way: 
+ * no efficient algorithm can distinguish (with significant advantage) between a function chosen randomly from the PRF family and a random oracle 
+ * (a function whose outputs are fixed completely at random).
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
  */
 public interface PseudorandomFunction {
 	/**
@@ -34,7 +35,7 @@ public interface PseudorandomFunction {
 	public void init(SecretKey secretKey, AlgorithmParameterSpec params) throws InvalidParameterSpecException;
 	
 	/**
-	 * 
+	 * An object trying to use an instance of prf needs to check if it has already been initialized.
 	 * @return true if the object was initialized by calling the function init.
 	 */
 	public boolean isInitialized();
@@ -59,36 +60,35 @@ public interface PseudorandomFunction {
 	public String getAlgorithmName() ;
 
 	/** 
-	 * @return 
+	 * @return the input block size in bits
 	 */
 	public int getBlockSize() ;
 
 	/** 
-	 * computeBlock : computes the function using the secret key. The user supplies the input byte array and the offset from 
-	 * which to take the data from. The user also supplies the output byte array as well as the offset. 
-	 * The computeBlock function will put the output starting at the offset. This function is suitable for block ciphers where 
-	 * the input/output length is known in advance.
+	 * Computes the function using the secret key. <p>
+	 * The user supplies the input byte array and the offset from which to take the data from. 
+	 * The user also supplies the output byte array as well as the offset. 
+	 * The computeBlock function will put the output in the output array starting at the offset. <p> 
+	 * This function is suitable for block ciphers where the input/output length is known in advance.
 	 * @param inBytes input bytes to compute
 	 * @param inOff input offset in the inBytes array
 	 * @param outBytes output bytes. The resulted bytes of compute
-	 * @param outOff output offset in the outBytes array to take the result from
+	 * @param outOff output offset in the outBytes array to put the result from
 	 * @throws IllegalBlockSizeException 
 	 * @throws UnInitializedException 
 	 */
 	public void computeBlock(byte[] inBytes, int inOff, byte[] outBytes, int outOff) throws IllegalBlockSizeException, UnInitializedException;
 	
 	/**
-	 * 
-	 * computetBlock : This function is provided in the interface especially for the sub-family PrfVaryingInputLength
-	 * 
-	 * This function is necessary since some of the Prf's we implement may have variable input and output length.
-	 * If the implemented algorithm is a block cipher then the size of the input as well as the output is known in advence and 
+	 * Computes the function using the secret key. <p>
+	 * This function is provided in the interface especially for the sub-family PrfVaryingIOLength, which may have variable input and output length.
+	 * If the implemented algorithm is a block cipher then the size of the input as well as the output is known in advance and 
 	 * the use may call the other computeBlock function where length is not require.
 	 * @param inBytes input bytes to compute
-	 * @param inLen the length of the input array
 	 * @param inOff input offset in the inBytes array
+	 * @param inLen the length of the input array
 	 * @param outBytes output bytes. The resulted bytes of compute
-	 * @param outOff output offset in the outBytes array to take the result from
+	 * @param outOff output offset in the outBytes array to put the result from
 	 * @param outLen the length of the output array
 	 * @throws IllegalBlockSizeException 
 	 * @throws UnInitializedException 
@@ -96,21 +96,19 @@ public interface PseudorandomFunction {
 	public void computeBlock(byte[] inBytes, int inOff, int inLen, byte[] outBytes, int outOff, int outLen) throws IllegalBlockSizeException, UnInitializedException;
 	
 	/**
-	 * 
-	 * computetBlock : This function is provided in this PseudorandomFunction interface for the sake of interfaces (or classes) for which 
-	 * the input-output lengths can be different for each computation. Hmac and Prf/Prp with variable input/output length are examples of 
+	 * Computes the function using the secret key. <p>
+	 * This function is provided in this PseudorandomFunction interface for the sake of interfaces (or classes) for which 
+	 * the input length can be different for each computation. Hmac and Prf/Prp with variable input length are examples of 
 	 * such interfaces.
 	 * 
-	 * This function is necessary since some of the Prf's we implement may have only variable input and fixed output length.
 	 * @param inBytes input bytes to compute
-	 * @param inLen the length of the input array
 	 * @param inOff input offset in the inBytes array
+	 * @param inLen the length of the input array
 	 * @param outBytes output bytes. The resulted bytes of compute.
-	 * @param outOff output offset in the outBytes array to take the result from
+	 * @param outOff output offset in the outBytes array to put the result from
 	 * @throws IllegalBlockSizeException 
 	 * @throws UnInitializedException 
 	 */
-
 	public void computeBlock(byte[] inBytes, int inOffset, int inLen, byte[] outBytes, int outOffset) throws IllegalBlockSizeException, UnInitializedException;;
 
 	
