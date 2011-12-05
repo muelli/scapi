20e97547aac8f3698b641273af5278a919a66287
Moriya Farbstein <moriyaw@gmail.com>
2011-12-05 09:27:32 +0000

    1. fixed comments to match javadoc guideline 2. deleted the offset and length checks in computeBlock function that calls other computeBlock which does these checks.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@637 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/prf/bc/BcHMAC.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/prf/bc/BcHMAC.java
@@ -1,6 +1,3 @@
-/**
- * An adapter class that wrapps the Hmac of bouncy castle.
- */
 package edu.biu.scapi.primitives.prf.bc;
 
 import java.security.spec.AlgorithmParameterSpec;
@@ -23,31 +20,33 @@ import edu.biu.scapi.tools.Translation.BCParametersTranslator;
 
 
 
-	/** 
-	 * @author LabTest
-	 */
+/** 
+ * Adapter class that wraps the Hmac of bouncy castle.
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
+ */
 public final class BcHMAC implements  Hmac {
-	
+	/*
+	 * Our class Hmac is an adapter class for the adaptee class HMac of BC.  
+	 */
 	private HMac hMac;//The underlying wrapped hmac of BC.
 	private AlgorithmParameterSpec params = null;
 	private SecretKey secretKey = null;
-	private boolean isInitialized = false;//until isInitialized() is called set to false.
+	private boolean isInitialized = false;//until init is called set to false.
 
 	/** 
-	 * A constructor that can be called from the factory
+	 * This constructor receives an hashName and build the underlying hmac accoring to it. It can be called from the factory.
 	 * @param hashName - the hash function to translate into digest of bc hmac
 	 * @throws FactoriesException 
 	 */
 	public BcHMAC(String hashName) throws FactoriesException {
 		
 		hMac = new HMac(BCFactory.getInstance().getDigest(hashName));
-		
-		
+			
 	}
 
 	/**
-	 * A constructor that gets a SCAPI collision resistant hash and retrieves the name of the hash in
-	 * order to crete the related digest for the BC Hmac this class uses.
+	 * This constructor gets a SCAPI collision resistant hash to be the underlying hash and retrieves the name of the hash in
+	 * order to create the related digest for the BC Hmac this class uses.
 	 * @param hash - the underlying collision resistant hash 
 	 * @throws FactoriesException, IllegalStateException 
 	 */
@@ -56,7 +55,7 @@ public final class BcHMAC implements  Hmac {
 	
 		//first check that the hmac is initialized.
 		if(hash.isInitialized()){
-			//pass a digest to the KDF.
+			//passes a digest to the hmac.
 			hMac = new HMac(BCFactory.getInstance().getDigest(hash.getAlgorithmName()));
 		}
 		else{//the user must pass an initialized object, otherwise throw an exception
@@ -71,126 +70,49 @@ public final class BcHMAC implements  Hmac {
 	 * @throws InvalidParameterSpecException 
 	 */
 	public void init(SecretKey secretKey, AlgorithmParameterSpec params) throws InvalidParameterSpecException {
-		//no auxiliary parameters for HMAC. Pass the key
+		//no auxiliary parameters for HMAC. Passes the key
 		init(secretKey);
 	}
 	
 	/** 
-	 * Initializes the bc underlying hmac
-	 * @param secretKey the secret key to convert to bc key parameter
+	 * Initializes this hmac with a secret key.
+	 * @param secretKey the secret key 
 	 */
 	public void init(SecretKey secretKey) {
 		
-		//set flag to true. Object is initializing.
-		isInitialized = true;
-		
-		//assign the key
+		//assigns the key
 		this.secretKey = secretKey;
 		
 		CipherParameters bcParams; 
-		//get the relevant cipher parameter
+		//gets the relevant BC cipher parameter
 		bcParams = BCParametersTranslator.getInstance().translateParameter((SecretKeySpec)secretKey);
 		
-		//pass the key parameter to bc hmac
+		//passes the key parameter to bc hmac
 		hMac.init(bcParams);
+		
+		//sets flag to true. Object is initializing.
+		isInitialized = true;
 		
 	}
 	
-	/**
-	 * 
-	 * @return true if the object was initialized by calling the function init.
-	 */
 	public boolean isInitialized(){
 		return isInitialized;
 	}
 
-	/** 
-	 * Since the input is not fixed, it must be supplied. This function should not be called. 
-	 * If the user still calls this function, throw an exception.
-	 * @param inBytes input bytes to compute
-	 * @param inOff input offset in the inBytes array
-	 * @param outBytes output bytes. The resulted bytes of compute
-	 * @param outOff output offset in the outBytes array to take the result from
-	 * @throws IllegalBlockSizeException 
-	 * @throws UnInitializedException 
-	 */
-	public void computeBlock(byte[] inBytes, int inOff, byte[] outBytes, int outOff) throws IllegalBlockSizeException, UnInitializedException{
+	public AlgorithmParameterSpec getParams() throws UnInitializedException {
 		if(!isInitialized()){
 			throw new UnInitializedException();
 		}
-		throw new IllegalBlockSizeException("Size of input is not specified");
+		return params;
 	}
-	
-	/**
-	 * 
-	 * Since both output variable is fixed this function should not normally be called. 
-	 * If the user still wants to use this function, the specified argument outLen should be the same as 
-	 * the result of getMacSize from BC, otherwise, throw an exception. 
-	 * 
-	 * @param inBytes input bytes to compute
-	 * @param inLen the length of the input array
-	 * @param inOff input offset in the inBytes array
-	 * @param outBytes output bytes. The resulted bytes of compute
-	 * @param outOff output offset in the outBytes array to take the result from
-	 * @param outLen the length of the output array
-	 * @throws IllegalBlockSizeException 
-	 * @throws UnInitializedException 
-	 */
-	public void computeBlock(byte[] inBytes, int inOff, int inLen, byte[] outBytes, int outOff, int outLen) throws IllegalBlockSizeException, UnInitializedException{
+
+	public SecretKey getSecretKey() throws UnInitializedException {
 		if(!isInitialized()){
 			throw new UnInitializedException();
 		}
-		// check that the offset and length are correct 
-		if ((inOff > inBytes.length) || (inOff+inLen > inBytes.length)){
-			throw new ArrayIndexOutOfBoundsException("wrong offset for the given input buffer");
-		}
-		if ((outOff > outBytes.length) || (outOff+outLen > outBytes.length)){
-			throw new ArrayIndexOutOfBoundsException("wrong offset for the given output buffer");
-		}
-		//make sure the output size is correct
-		if(outLen==hMac.getMacSize())
-			computeBlock(inBytes, inOff, inLen, outBytes, outOff);
-		else
-			throw new IllegalBlockSizeException("Output size is incorrect");
+		return secretKey;
 	}
 	
-	/**
-	 * 
-	 * Computes the function using the secret key. The user supplies the input byte array and the offset from 
-	 * which to take the data from. Also since the input is not fixed the input is supplied as well. 
-	 * The user also supplies the output byte array as well as the offset. 
-	 * The computeBlock function will put the output starting at the offset. 
-	 * 
-	 * @param inBytes input bytes to compute
-	 * @param inLen the length of the input array
-	 * @param inOffset input offset in the inBytes array
-	 * @param outBytes output bytes. The resulted bytes of compute
-	 * @param outOffset output offset in the outBytes array to take the result from
-	 * @throws UnInitializedException 
-	 */
-
-	public void computeBlock(byte[] inBytes, int inOffset, int inLen,
-			byte[] outBytes, int outOffset) throws IllegalBlockSizeException, UnInitializedException {
-		if(!isInitialized()){
-			throw new UnInitializedException();
-		}
-		// check that the offset and length are correct 
-		if ((inOffset > inBytes.length) || (inOffset+inLen > inBytes.length)){
-			throw new ArrayIndexOutOfBoundsException("wrong offset for the given input buffer");
-		}
-		if ((outOffset > outBytes.length) || (outOffset+getBlockSize() > outBytes.length)){
-			throw new ArrayIndexOutOfBoundsException("wrong offset for the given output buffer");
-		}
-		//pass the input bytes to update
-		hMac.update(inBytes, inOffset, inLen);
-		
-		//get the output results through doFinal
-		hMac.doFinal(outBytes, outOffset);
-	}
-	
-
-	
-
 	/** 
 	 * @return the name from BC hmac
 	 */
@@ -206,30 +128,80 @@ public final class BcHMAC implements  Hmac {
 	public int getBlockSize(){
 		return hMac.getMacSize();
 	}
-
-	/** 
-	 * @return the parameters spec
-	 * @throws UnInitializedException 
-	 */
-	public AlgorithmParameterSpec getParams() throws UnInitializedException {
-		if(!isInitialized()){
-			throw new UnInitializedException();
-		}
-		return params;
-	}
-
-
-
-	/**
-	 * @return the secret key
-	 * @throws UnInitializedException 
-	 */
-	public SecretKey getSecretKey() throws UnInitializedException {
-		if(!isInitialized()){
-			throw new UnInitializedException();
-		}
-		return secretKey;
-	}
-
 	
+	/** 
+	 * This function is provided in the interface especially for the sub-family Prp, which have fixed input/output lengths.
+	 * Since in this case the input is not fixed, it must be supplied and this function should not be called. 
+	 * If the user still calls this function, throws an exception.
+	 * @throws IllegalBlockSizeException 
+	 * @throws UnInitializedException 
+	 */
+	public void computeBlock(byte[] inBytes, int inOff, byte[] outBytes, int outOff) throws IllegalBlockSizeException, UnInitializedException{
+		if(!isInitialized()){
+			throw new UnInitializedException();
+		}
+		throw new IllegalBlockSizeException("Size of input is not specified");
+	}
+	
+	/**
+	 * This function is provided in the interface especially for the sub-family PrfVaryingIOLength, which have varying input/output lengths.
+	 * Since in this case the output variable is fixed this function should not normally be called. 
+	 * If the user still wants to use this function, the specified argument outLen should be the same as 
+	 * the result of getMacSize from BC, otherwise, throws an exception. 
+	 * 
+	 * @param inBytes input bytes to compute
+	 * @param inLen the length of the input array
+	 * @param inOff input offset in the inBytes array
+	 * @param outBytes output bytes. The resulted bytes of compute
+	 * @param outOff output offset in the outBytes array to put the result from
+	 * @param outLen the length of the output array
+	 * @throws IllegalBlockSizeException 
+	 * @throws UnInitializedException 
+	 */
+	public void computeBlock(byte[] inBytes, int inOff, int inLen, byte[] outBytes, int outOff, int outLen) throws IllegalBlockSizeException, UnInitializedException{
+		if(!isInitialized()){
+			throw new UnInitializedException();
+		}
+		//the checks of the offsets and lengths is done in the conputeBlock (inBytes, inOff, inLen, outBytes, outOff)
+		//make sure the output size is correct
+		if(outLen==hMac.getMacSize())
+			computeBlock(inBytes, inOff, inLen, outBytes, outOff);
+		else
+			throw new IllegalBlockSizeException("Output size is incorrect");
+	}
+	
+	/**
+	 * Computes the function using the secret key. 
+	 * The user supplies the input byte array and the offset from 
+	 * which to take the data from. Also since the input is not fixed the input length is supplied as well. 
+	 * The user also supplies the output byte array as well as the offset. 
+	 * The computeBlock function will put the output starting at the offset. 
+	 * 
+	 * @param inBytes input bytes to compute
+	 * @param inLen the length of the input array
+	 * @param inOffset input offset in the inBytes array
+	 * @param outBytes output bytes. The resulted bytes of compute
+	 * @param outOffset output offset in the outBytes array to put the result from
+	 * @throws UnInitializedException 
+	 */
+
+	public void computeBlock(byte[] inBytes, int inOffset, int inLen,
+			byte[] outBytes, int outOffset) throws UnInitializedException {
+		if(!isInitialized()){
+			throw new UnInitializedException();
+		}
+		// checks that the offset and length are correct 
+		if ((inOffset > inBytes.length) || (inOffset+inLen > inBytes.length)){
+			throw new ArrayIndexOutOfBoundsException("wrong offset for the given input buffer");
+		}
+		if ((outOffset > outBytes.length) || (outOffset+getBlockSize() > outBytes.length)){
+			throw new ArrayIndexOutOfBoundsException("wrong offset for the given output buffer");
+		}
+		//passes the input bytes to update
+		hMac.update(inBytes, inOffset, inLen);
+		
+		//gets the output results through doFinal
+		hMac.doFinal(outBytes, outOffset);
+	}
+
 }
