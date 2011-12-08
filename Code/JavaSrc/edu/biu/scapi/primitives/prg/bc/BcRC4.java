1e76978676fb44fe7d6bee554a3ba81a158faaab
Moriya Farbstein <moriyaw@gmail.com>
2011-12-08 10:37:46 +0000

    fixed comments
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@678 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/prg/bc/BcRC4.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/prg/bc/BcRC4.java
@@ -1,17 +1,20 @@
-/**
- * 
- */
 package edu.biu.scapi.primitives.prg.bc;
 
 import org.bouncycastle.crypto.engines.RC4Engine;
 
 import edu.biu.scapi.primitives.prg.RC4;
 
-/** 
- * @author LabTest
+/**
+ * Concrete class of PRF for RC4. This class is a wrapper class for BC implementation of RC4.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
+ *
  */
 public final class BcRC4 extends BcPRG implements RC4{
 	
+	/**
+	 * Passes the RC4Engine of BC to the abstract super class
+	 */
 	public BcRC4(){
 		super(new RC4Engine());
 	}
