8b403afbacd8f68e2fc69b7f71155936856c1337
Moriya Farbstein <moriyaw@gmail.com>
2011-12-08 10:37:22 +0000

    1. added the function isInitialized to the PRG interface 2. fixed comments
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@676 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/prg/PseudorandomGenerator.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/prg/PseudorandomGenerator.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package edu.biu.scapi.primitives.prg;
 
 import java.security.spec.AlgorithmParameterSpec;
@@ -10,7 +7,13 @@ import javax.crypto.SecretKey;
 import edu.biu.scapi.exceptions.UnInitializedException;
 
 /** 
- * @author LabTest
+ * General interface of pseudorandom generator. Every concrete class in this family should implement this interface. <p>
+ * 
+ * A pseudorandom generator (PRG) is a deterministic algorithm that takes a “short” uniformly distributed string, 
+ * known as the seed, and outputs a longer string that cannot be efficiently distinguished from a uniformly 
+ * distributed string of that length.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
   */
 public interface PseudorandomGenerator {
 	/** 
@@ -21,33 +24,42 @@ public interface PseudorandomGenerator {
 
 	/** 
 	 * Initializes this prg with the secret key and the auxiliary parameters
-	 * @param secretKey
-	 * @param params
+	 * @param secretKey secret key
+	 * @param params the algorithm auxiliary parameters
 	 */
 	public void init(SecretKey secretKey, AlgorithmParameterSpec params);
 
+	/**
+	 * An object trying to use an instance of prg needs to check if it has already been initialized.
+	 * @return true if the object was initialized by calling the function init.
+	 */
+	public boolean isInitialized();
+	
 	/** 
-	 * @return the parameter spec of this prg
-	 * @throws UnInitializedException 
+	 * @return the parameter spec of this PRG
+	 * @throws UnInitializedException if this object is not initialized
 	 */
 	public AlgorithmParameterSpec getParams() throws UnInitializedException;
 
 	/** 
-	 * @return the algorithm name
+	 * @return the secret key of this PRG
+	 * @throws UnInitializedException if this object is not initialized
+	 */
+	public SecretKey getSecretKey() throws UnInitializedException;
+	
+	/** 
+	 * @return the algorithm name. For example - RC4
 	 */
 	public String getAlgorithmName();
 
 	/**
+	 * Streams the prg bytes.
 	 * @param outBytes - output bytes. The result of streaming the bytes.
 	 * @param outOffset - output offset
-	 * @param outlen - length
-	 * @throws UnInitializedException 
+	 * @param outlen - the required output length
+	 * @throws UnInitializedException if this object is not initialized
 	 */
 	public void getPRGBytes(byte[] outBytes, int outOffset, int outlen) throws UnInitializedException;
 
-	/** 
-	 * @return the secret key
-	 * @throws UnInitializedException 
-	 */
-	public SecretKey getSecretKey() throws UnInitializedException;
+	
 }
