4268f87486796859686dd797c58b251b9db4e032
Moriya Farbstein <moriyaw@gmail.com>
2011-12-11 09:21:50 +0000

    1. added function generateKey that gets an iv and a key as byte array with offset and length and returns the generated key as byte array 2. 2. fixed comments
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@679 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/kdf/KeyDerivationFunction.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/kdf/KeyDerivationFunction.java
@@ -1,8 +1,3 @@
-/**
- * 
- * A key derivation function (or KDF) is used to derive (close to) uniformly distributed string/s from a secret value 
- * with high entropy (but no other guarantee regarding its distribution). 
- */
 package edu.biu.scapi.primitives.kdf;
 
 import java.security.spec.AlgorithmParameterSpec;
@@ -12,36 +7,74 @@ import javax.crypto.SecretKey;
 import edu.biu.scapi.exceptions.UnInitializedException;
 
 /** 
-  * @author LabTest
+ * General interface of key derivation function. Every class in this family should implement this interface. <p>
+ * A key derivation function (or KDF) is used to derive (close to) uniformly distributed string/s from a secret value 
+ * with high entropy (but no other guarantee regarding its distribution). 
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
  */
 public interface KeyDerivationFunction {
-	/** 
-
-	 * @param key
-	 * @param len
-	 * @return
-	 * @throws UnInitializedException 
-	 */
-	public SecretKey generateKey(SecretKey key, int outLen,  byte[] iv) throws UnInitializedException;
-	public SecretKey generateKey(SecretKey key, int outLen) throws UnInitializedException;
-	public void generateKey(byte[] inKey, int inOff, int inLen, byte[] outKey, int outOff, int outLen) throws UnInitializedException;
 	
 	/**
-	 * Initializes this krf with the secret key.
-	 * @param secretKey the secrete key
+	 * Initializes this kdf with the secret key. The initialization is for the underlying object the KDF used.
+	 * @param secretKey the secret key
 	 *  */
 	public void init(SecretKey secretKey);
 
 	/** 
-	 * Initializes this krf with the secret key and the auxiliary parameters.
+	 * Initializes this kdf with the secret key and the auxiliary parameters. The initialization is for the underlying object the KDF used.
 	 * @param secretKey secret key
 	 * @param params algorithm parameters
 	 */
 	public void init(SecretKey secretKey, AlgorithmParameterSpec params);
 	
 	/**
-	 * 
+	 * An object trying to use an instance of kdf needs to check if it has already been initialized.
 	 * @return true if the object was initialized by calling the function init.
 	 */
 	public boolean isInitialized();
+	
+	/** 
+	 * Generates a new secret key from the given seed and IV.
+	 * @param seedForGeneration the secret key that is the seed for the key generation
+	 * @param len the required output key length
+	 * @param iv info for the key generation
+	 * @return secret key the generated key
+	 * @throws UnInitializedException if this object is not initialized
+	 */
+	public SecretKey generateKey(SecretKey seedForGeneration, int outLen,  byte[] iv) throws UnInitializedException;
+	
+	/** 
+	 * Generates a new secret key from the given seed.
+	 * @param seedForGeneration the secret key that is the seed for the key generation
+	 * @param len the required output key length
+	 * @return secret key the generated key
+	 * @throws UnInitializedException if this object is not initialized
+	 */
+	public SecretKey generateKey(SecretKey seedForGeneration, int outLen) throws UnInitializedException;
+	
+	/** 
+	 * Generates a new secret key from the given seed.
+	 * @param seedForGeneration the secret key that is the seed for the key generation
+	 * @param inOff the offset within the seedForGeneration to take the bytes from
+	 * @param inLen the length of the seed
+	 * @param outKey the array to put the generated key bytes
+	 * @param outoff the offset within the output array to put the generated key bytes from
+	 * @param outlen the required output key length
+	 * @throws UnInitializedException if this object is not initialized
+	 */
+	public void generateKey(byte[] seedForGeneration, int inOff, int inLen, byte[] outKey, int outOff, int outLen) throws UnInitializedException;
+	
+	/** 
+	 * Generates a new secret key from the given seed and iv.
+	 * @param seedForGeneration the secret key that is the seed for the key generation
+	 * @param inOff the offset within the seedForGeneration to take the bytes from
+	 * @param inLen the length of the seed
+	 * @param outKey the array to put the generated key bytes
+	 * @param outoff the offset within the output array to put the generated key bytes from
+	 * @param outlen the required output key length
+	 * @param iv info for the key generation
+	 * @throws UnInitializedException if this object is not initialized
+	 */
+	public void generateKey(byte[] seedForGeneration, int inOff, int inLen, byte[] outKey, int outOff, int outLen, byte[] iv) throws UnInitializedException;
 }
