c647c8d0e489b857ebb493e1147fc9174bdbbcaa
Moriya Farbstein <moriyaw@gmail.com>
2011-12-13 12:13:48 +0000

    1. added comments 2. added the function getSecretKey
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@699 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/perfectUniversalHash/UniversalHash.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/perfectUniversalHash/UniversalHash.java
@@ -1,4 +1,15 @@
-/**
+package edu.biu.scapi.primitives.universalHash;
+
+import java.security.spec.AlgorithmParameterSpec;
+
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
+
+import edu.biu.scapi.exceptions.UnInitializedException;
+
+/** 
+ * General interface for perfect universal hash. Every class in this family should implement this interface.
+ * 
  * A universal hash function is a family of hash functions (as in the sense of hash functions for data structures) with the property that 
  * a randomly chosen hash function (from the family) yields very few collisions, with good probability. 
  * More importantly in a cryptographic context, universal hash functions have important properties, like good randomness extraction and 
@@ -7,25 +18,15 @@
  * have a similar name). We therefore completely separate the two implementations so that collision-resistant hash functions cannot be confused 
  * with perfect universal hash functions.
  * 
- * The input and output lengths of a perfect universal hash function are fixed for any given instantiation, and are set upon init.
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
  */
-package edu.biu.scapi.primitives.perfectUniversalHash;
-
-import java.security.spec.AlgorithmParameterSpec;
-
-import javax.crypto.SecretKey;
-
-import edu.biu.scapi.exceptions.UnInitializedException;
-
-/** 
- @author LabTest
- */
-public interface PerfectUniversalHash {
+public interface UniversalHash {
 	
 	/**
 	 * Initializes this PerfectUniversalHash with the secret key.
-	 * @param secretKey the secrete key
-	 *  */
+	 * @param secretKey the secret key
+	 *  
+	 */
 	public void init(SecretKey secretKey);
 
 	/** 
@@ -36,16 +37,22 @@ public interface PerfectUniversalHash {
 	public void init(SecretKey secretKey, AlgorithmParameterSpec params);
 
 	/**
-	 * 
+	 * An object trying to use an instance of perfectUniversalHash needs to check if it has already been initialized.
 	 * @return true if the object was initialized by calling the function init.
 	 */
 	public boolean isInitialized();
 	
 	/** 
 	 * @return the parameter spec of this perfect universal hash
-	 * @throws UnInitializedException 
+	 * @throws UnInitializedException if this object is not initialized
 	 */
 	public AlgorithmParameterSpec getParams() throws UnInitializedException;
+	
+	/** 
+	 * @return the secret key of this perfect universal hash
+	 * @throws UnInitializedException if this object is not initialized
+	 */
+	public SecretKey getSecretKey() throws UnInitializedException;
 
 	/** 
 	 * @return the algorithm name
@@ -53,6 +60,15 @@ public interface PerfectUniversalHash {
 	public String getAlgorithmName();
 
 	/** 
+	 * This function has multiple roles depending on the concrete hash function.
+	 * If the concrete class can get a varying input lengths then there are 2 possible answers:
+	 * 1. The maximum size of the input – if there is some kind of an upper bound on the input size 
+	 * (for example in the EvaluationHashFunction there is a limit on the input size due to security reasons) 
+	 * thus, this function returns this bound even though the actual size can be any number between zero and that limit.
+	 * 2. Zero – if there is no limit on the input than this function returns 0.
+	 * If the concrete class can get a fixed length, this function returns a constant size that may be determined either in the init 
+	 * for some implementations or hardcoded for other implementations.
+
 	 * @return the input size of this hash function
 	 */
 	public int getInputSize();
@@ -63,14 +79,15 @@ public interface PerfectUniversalHash {
 	public int getOutputSize();
 
 	/** 
-	 * Compute the hash function on the in byte array and put the result in the output byte array
+	 * Computes the hash function on the in byte array and put the result in the output byte array
 	 * @param in - input byte array
 	 * @param inOffset - the offset within the input byte array
 	 * @param inLen - length. The number of bytes to take after the offset
 	 * @param out - output byte array
 	 * @param outOffset - the offset within the output byte array
-	 * @throws UnInitializedException 
+	 * @throws UnInitializedException if this object is not initialized
+	 * @throws IllegalBlockSizeException if the input length is greater than the upper limit
 	 */
-	public void compute(byte[] in, int inOffset, byte[] out,
-			int outOffset) throws UnInitializedException;
+	public void compute(byte[] in, int inOffset, int inLen, byte[] out,
+			int outOffset) throws UnInitializedException, IllegalBlockSizeException;
 }
