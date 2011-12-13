5011b126b10acd8dd4752cb9950cf885c3e8bf93
Moriya Farbstein <moriyaw@gmail.com>
2011-12-13 11:56:46 +0000

    deleted import of tpElement since this package has been deleted and the files transfered to the package trapdoorPermutation
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@693 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/tools/Factories/TPElementFactory.java
+++ Code/JavaSrc/edu/biu/scapi/tools/Factories/TPElementFactory.java
@@ -3,7 +3,7 @@ package edu.biu.scapi.tools.Factories;
 import java.math.BigInteger;
 
 import edu.biu.scapi.exceptions.FactoriesException;
-import edu.biu.scapi.primitives.trapdoorPermutation.TPElement.TPElement;
+import edu.biu.scapi.primitives.trapdoorPermutation.TPElement;
 
 public class TPElementFactory {
 
