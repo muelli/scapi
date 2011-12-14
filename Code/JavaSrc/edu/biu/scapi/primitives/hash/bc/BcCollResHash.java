6c6a666b0e85e4c73f3567ba85ed12ed4bb390d4
Moriya Farbstein <moriyaw@gmail.com>
2011-12-14 12:51:39 +0000

    1. instead of extends the class CollisionResistantAbs, implements the new interface CryptographicHash since the abstract class has been deleted 2. fixed comments 3. added the function isinitialized
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@711 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/hash/bc/BcCollResHash.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/hash/bc/BcCollResHash.java
@@ -6,35 +6,40 @@ package edu.biu.scapi.primitives.hash.bc;
 import org.bouncycastle.crypto.Digest;
 
 import edu.biu.scapi.exceptions.UnInitializedException;
-import edu.biu.scapi.primitives.hash.TargetCollisionResistantAbs;
+import edu.biu.scapi.primitives.hash.CryptographicHash;
 
 /** 
- * @author LabTest
- * 
- * A general adapter class of hash for Bouncy Castle. 
+ * A general adapter class of hash for Bouncy Castle. <p>
  * This class implements all the functionality by passing requests to the adaptee interface Digest. 
  * A concrete hash function such as SHA1 represented by the class BcSHA1 only passes the SHA1Digest object in the constructor 
  * to the base class. 
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
+ * 
  */
-public abstract class BcCollResHash extends TargetCollisionResistantAbs {
-	private Digest digest;
+public abstract class BcCollResHash implements CryptographicHash {
+	private Digest digest; //the underlying digest
 	
 	 /**
-	 * @param digest the underlying digest of BC
-	 */
+	  * Sets the underlying digest
+	  * @param digest the underlying digest of BC
+	  */
 	public BcCollResHash(Digest digest) {
 	
-		//set the underlying bc digest
+		//sets the underlying bc digest
 		this.digest = digest;
 	}
 
+	public boolean isInitialized(){
+		return true;
+	}
 	
 	/** 
 	 * @return the algorithm name taken from BC
 	 */
 	public String getAlgorithmName() {
 	
-		//get the name from the digest of BC
+		//gets the name from the digest of BC
 		return digest.getAlgorithmName();
 	}
 
@@ -43,7 +48,7 @@ public abstract class BcCollResHash extends TargetCollisionResistantAbs {
 	 */
 	public int getHashedMsgSize() {
 		
-		//get the size from the underlying digest
+		//gets the size from the underlying digest
 		return digest.getDigestSize();
 	}
 
@@ -52,37 +57,37 @@ public abstract class BcCollResHash extends TargetCollisionResistantAbs {
 	 * @param in input byte array
 	 * @param inOffset the offset within the byte array
 	 * @param inLen the length. The number of bytes to take after the offset
-	 * @throws UnInitializedException 
+	 * @throws UnInitializedException if this object is not initialized
 	 * */
 	public void update(byte[] in, int inOffset, int inLen) throws UnInitializedException {
-		//check that the object is initialized
+		//checks that the object is initialized
 		if (!isInitialized()){
 			throw new UnInitializedException();
 		}
-		//check that the offset and length are correct
+		//checks that the offset and length are correct
 		if ((inOffset > in.length) || (inOffset+inLen > in.length)){
 			throw new ArrayIndexOutOfBoundsException("wrong offset for the given input buffer");
 		}
-		//delegate the update request to the underlying digest
+		//delegates the update request to the underlying digest
 		digest.update(in, inOffset, inLen);
 	}
 
 	/** 
 	 * Completes the hash computation and puts the result in the out array.
 	 * @param out the output in byte array
-	 * @param outOffset the offset from which to take bytes from
-	 * @throws UnInitializedException 
+	 * @param outOffset the offset which to put the result bytes from
+	 * @throws UnInitializedException if this object is not initialized
 	 */
 	public void hashFinal(byte[] out, int outOffset) throws UnInitializedException {
-		//check that the object is initialized
+		//checks that the object is initialized
 		if (!isInitialized()){
 			throw new UnInitializedException();
 		}
-		//check that the offset and length are correct
+		//checks that the offset and length are correct
 		if ((outOffset > out.length) || (outOffset+getHashedMsgSize() > out.length)){
 			throw new ArrayIndexOutOfBoundsException("wrong offset for the given output buffer");
 		}
-		//delegate the update request to the underlying digest by calling it's function doFinal. This function
+		//delegates the update request to the underlying digest by calling it's function doFinal. This function
 		//will update the out array.
 		digest.doFinal(out, outOffset);
 	}
