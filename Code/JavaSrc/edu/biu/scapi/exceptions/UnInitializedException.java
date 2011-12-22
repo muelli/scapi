05cc196559dea45edbb65cda1b322ca04bb105ec
Moriya Farbstein <moriyaw@gmail.com>
2011-12-22 06:43:26 +0000

    added constructor that gets a message
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@758 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/exceptions/UnInitializedException.java
+++ Code/JavaSrc/edu/biu/scapi/exceptions/UnInitializedException.java
@@ -6,8 +6,17 @@ public class UnInitializedException extends Exception{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	private String msg = null;
+	
+	public UnInitializedException(String message){
+		msg = message;
+	}
+	
+	public UnInitializedException(){
+		msg = "cannot perform any function before initialization";
+	}
+	
 	public String getMessage(){
-		return "cannot perform any function before initialization";
+		return msg;
 	}
 }
