79c46816c5120956413d0676341925be576b1569
Moriya Farbstein <moriyaw@gmail.com>
2012-01-05 12:49:39 +0000

    add a serialVersionUID
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@797 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/trapdoorPermutation/ScRabinKey.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/trapdoorPermutation/ScRabinKey.java
@@ -10,6 +10,7 @@ import java.math.BigInteger;
  */
 public abstract class ScRabinKey implements RabinKey{
 
+	private static final long serialVersionUID = 1L;
 	protected BigInteger modulus = null;
 	
 	/**
