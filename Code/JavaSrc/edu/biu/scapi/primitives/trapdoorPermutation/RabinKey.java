821a40d14cb6c26b4dcf9a588b8e6282b6772dae
Moriya Farbstein <moriyaw@gmail.com>
2012-01-05 12:50:30 +0000

    changed to extend from Key interface
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@798 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/trapdoorPermutation/RabinKey.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/trapdoorPermutation/RabinKey.java
@@ -1,13 +1,14 @@
 package edu.biu.scapi.primitives.trapdoorPermutation;
 
 import java.math.BigInteger;
+import java.security.Key;
 
 /**
  * General interface for Rabin Keys
  * 
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  */
-public interface RabinKey {
+public interface RabinKey extends Key{
 	
 	/**
 	 * @return BigInteger - the modulus
