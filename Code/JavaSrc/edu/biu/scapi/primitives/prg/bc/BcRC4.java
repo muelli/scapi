a643152ad185fe78421ea103d27575f4b4b9a6d4
Moriya Farbstein <moriyaw@gmail.com>
2012-01-25 09:19:31 +0000

    RC4 has problem in the first processed 1024 bits. To avoid this problem, added init function that after initialization gets the first 1024 generated bits and ignore them.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@872 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/prg/bc/BcRC4.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/prg/bc/BcRC4.java
@@ -1,7 +1,14 @@
 package edu.biu.scapi.primitives.prg.bc;
 
+import java.security.spec.AlgorithmParameterSpec;
+import java.util.logging.Level;
+
+import javax.crypto.SecretKey;
+
 import org.bouncycastle.crypto.engines.RC4Engine;
 
+import edu.biu.scapi.exceptions.UnInitializedException;
+import edu.biu.scapi.generals.Logging;
 import edu.biu.scapi.primitives.prg.RC4;
 
 /**
@@ -17,5 +24,35 @@ public final class BcRC4 extends BcPRG implements RC4{
 	 */
 	public BcRC4(){
 		super(new RC4Engine());
+	}
+	
+	public void init(SecretKey secretKey) {
+		
+		//sets the parameters
+		super.init(secretKey);
+		
+		//RC4 has a problem in the first 1024 bits. by ignoring these bytes, we bypass this problem.
+		byte[] out = new byte[128];
+		try {
+			getPRGBytes(out, 0, 128);
+		} catch (UnInitializedException e) {
+			// shouldn't occur since super class initialized this prg
+			Logging.getLogger().log(Level.WARNING, e.toString());
+		}
+	}
+
+	public void init(SecretKey secretKey, AlgorithmParameterSpec params) {
+		
+		//sets the parameters
+		super.init(secretKey, params);
+		
+		//RC4 has a problem in the first 1024 bits. by ignoring these bytes, we bypass this problem.
+		byte[] out = new byte[128];
+		try {
+			getPRGBytes(out, 0, 128);
+		} catch (UnInitializedException e) {
+			// shouldn't occur since super class initialized this prg
+			Logging.getLogger().log(Level.WARNING, e.toString());
+		}
 	}
 }
