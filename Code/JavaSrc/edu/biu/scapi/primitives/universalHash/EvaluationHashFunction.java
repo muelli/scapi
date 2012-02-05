3fa872bc07c491b78dd661029d3d17fe7247c0fd
Moriya Farbstein <moriyaw@gmail.com>
2012-02-05 09:52:26 +0000

    1. added the padding functionality 2. changed comment of get inputSize function from returning 64 bits to 8 bytes
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@926 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/universalHash/EvaluationHashFunction.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/universalHash/EvaluationHashFunction.java
@@ -5,7 +5,13 @@ import java.security.spec.AlgorithmParameterSpec;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.SecretKey;
 
+import edu.biu.scapi.exceptions.FactoriesException;
 import edu.biu.scapi.exceptions.UnInitializedException;
+import edu.biu.scapi.paddings.BitPadding;
+import edu.biu.scapi.paddings.NoPadding;
+import edu.biu.scapi.paddings.PaddingParameterSpec;
+import edu.biu.scapi.paddings.PaddingScheme;
+import edu.biu.scapi.tools.Factories.PaddingFactory;
 
 /** 
  * Concrete class of perfect universal hash for evaluation hash function.
@@ -15,7 +21,7 @@ import edu.biu.scapi.exceptions.UnInitializedException;
 public final class EvaluationHashFunction extends UniversalHashAbs {
 	
 	protected long evalHashPtr; // pointer to the native evaluation object
-	
+	private PaddingScheme padding;
 	//native functions. These functions are implemented in the NTLJavaInterface dll using the JNI
 	
 	//creates the native object and initializes it with the secret key
@@ -34,11 +40,24 @@ public final class EvaluationHashFunction extends UniversalHashAbs {
 		
 		//sets the key
 		super.init(secretKey);
+		
+		padding = new BitPadding();
 	}
 	
-	public void init(SecretKey secretKey, AlgorithmParameterSpec params) {
-
-		init(secretKey);
+	public void init(SecretKey secretKey, AlgorithmParameterSpec params) throws FactoriesException {
+		
+		//passes the key to the native function, which creates a native evaluation hash function instance.
+		//the return value is the pointer to this instance, which we set to the class member evalHashPtr
+		evalHashPtr = initHash(secretKey.getEncoded(), 0);
+		
+		if (params instanceof PaddingParameterSpec){
+			padding = PaddingFactory.getInstance().getObject(((PaddingParameterSpec) params).getPaddingName());
+		} else {
+			padding = new BitPadding();
+		}
+		
+		//sets the parameters
+		super.init(secretKey, params);	
 	}
 	
 	/**
@@ -56,7 +75,7 @@ public final class EvaluationHashFunction extends UniversalHashAbs {
 	}
 
 	/** 
-	 * @return the output size of evaluation hash function - 64 bits
+	 * @return the output size of evaluation hash function - 8 bytes.
 	 */
 	public int getOutputSize() {
 		
@@ -92,83 +111,49 @@ public final class EvaluationHashFunction extends UniversalHashAbs {
 			throw new IllegalBlockSizeException("input length must be less than 64*(2^24-1) bits long");
 		}
 		
-		byte[] padding = null;
+		byte[] paddedArray = null;
 		//pad the input.
 		if ((inLen%8) == 0){
 			//the input is aligned to 64 bits so pads it as aligned array
-			padding = padAlignedArray(in, inOffset, inLen);
+			paddedArray = pad(in, inOffset, inLen, 8);
 		} else {
+			if (padding instanceof NoPadding){
+				throw new IllegalArgumentException("input is not aligned to blockSize");
+			}
+			//gets the number of bytes to add in order to get an aligned array
+			int inputSizeMod8 = inLen % 8;
+			int leftToAlign = 8 - inputSizeMod8;
 			//the input is not aligned to 64 bits so pads it to aligned array
-			padding = padNotAlignedArray(in, inOffset, inLen);
+			paddedArray = pad(in, inOffset, inLen, leftToAlign);
 		}
+		
 		//calls the native function compute on the padded array.
-		computeFunction(evalHashPtr, padding, out, outOffset);
+		computeFunction(evalHashPtr, paddedArray, out, outOffset);
 	}
 	
 	/**
 	 * This padding is used to get an array aligned to 8 bytes (64 bits).
-	 * The padding is done by adding 10...0 until we get the number of the required bytes.
+	 * The padding is done by calling the padding scheme class member to pad the array.
 	 * The input for this function is an array of size that is not aligned to 8 bytes.
 	 * @param input the input to pad. 
 	 * @param offset the offset to take the input bytes from
 	 * @param length the length of the input. This length is not aligned to 8 bytes.
 	 * @return the aligned array
 	 */
-	private byte[] padNotAlignedArray(byte[] input, int offset, int length){
-		//gets the number of bytes to add in order to get an aligned array
-		int inputSizeMod8 = length % 8;
-		int leftToAlign = 8 - inputSizeMod8;
-		
-		//creates an array of aligned size
-		byte[] alignedInput = new byte[length+leftToAlign];
-		
-		//copies the given input to the beginning of the aligned array
-		System.arraycopy(input, offset, alignedInput, 0, length);
-		//adds the first byte of the padding the byte that represent the number 1
-		Integer one = new Integer(1);
-		alignedInput[length] = one.byteValue();
-		
-		//decreases the number of bytes left to align
-		leftToAlign--;
-		
-		//adds zero bytes until reaches the required bytes 
-		Integer zero = new Integer(0);
-		for(int i=0; i<leftToAlign; i++){
-			alignedInput[length+1+i] = zero.byteValue();
-		}
-		return alignedInput;
-	}
-	
-	/**
-	 * All the inputs for the compute function need a padding. 
-	 * If the input is already aligned, the padding adds to it 8 bytes (64 bits) - 10000000.
-	 * @param input the input to pad. 
-	 * @param offset the offset to take the input bytes from
-	 * @param length the length of the input. This length is aligned to 8 bytes.
-	 * @return the aligned array
-	 */
-	private byte[] padAlignedArray(byte[] input, int offset, int length){
+	private byte[] pad(byte[] input, int offset, int length, int padSize){
 
-		//creates an array of input size + 8 bytes to the padding
-		byte[] alignedInput = new byte[length+8];
+		//copy the relevant part of the array to a new one
+		byte[] inputToPad = new byte[length];
+		System.arraycopy(input, offset, inputToPad, 0, length);
 		
-		//copies the given input to the beginning of the aligned array
-		System.arraycopy(input, offset, alignedInput, 0, length);
-		//adds the first byte of the padding the byte that represent the number 1
-		Integer one = new Integer(1);
-		alignedInput[length] = one.byteValue();
-		
-		//adds zero bytes until reaches the required bytes 
-		Integer zero = new Integer(0);
-		for(int i=0; i<7; i++){
-			alignedInput[length+1+i] = zero.byteValue();
-		}
-		return alignedInput;
+		//call the padding scheme to pad the array
+		return padding.pad(inputToPad, padSize);
 	}
 	
- static {
+	
+	static {
 		 
 		 //load the NTL jni dll
 		 System.loadLibrary("NTLJavaInterface");
-	 }
+	}
 }
