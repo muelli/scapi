ffe2092e5d601571ce72cd22e31125959a6a024b
Yael Ejgenberg <yaelnegje@yahoo.com>
2012-02-07 10:58:25 +0000

    Adding key infrastructure for Asymmetric Encryption.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@931 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/keys/CramerShoupPublicKey.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package edu.biu.scapi.midLayer.asymmetricCrypto.keys;
+
+import java.security.PublicKey;
+
+/**
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
+ *
+ */
+public class CramerShoupPublicKey implements PublicKey {
+
+	/* (non-Javadoc)
+	 * @see java.security.Key#getAlgorithm()
+	 */
+	@Override
+	public String getAlgorithm() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.security.Key#getEncoded()
+	 */
+	@Override
+	public byte[] getEncoded() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.security.Key#getFormat()
+	 */
+	@Override
+	public String getFormat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
