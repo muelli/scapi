5eb0c03e81234638644c52a2927b74be85fb158b
Moriya Farbstein <moriyaw@gmail.com>
2012-02-08 10:08:57 +0000

    added ElGamal keys interfaces
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@934 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/keys/ElGamalPrivateKey.java
@@ -0,0 +1,7 @@
+package edu.biu.scapi.midLayer.asymmetricCrypto.keys;
+
+import java.security.PrivateKey;
+
+public interface ElGamalPrivateKey extends PrivateKey {
+
+}
---
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/keys/ElGamalPublicKey.java
@@ -0,0 +1,7 @@
+package edu.biu.scapi.midLayer.asymmetricCrypto.keys;
+
+import java.security.PublicKey;
+
+public interface ElGamalPublicKey extends PublicKey {
+
+}
