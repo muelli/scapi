85dd59d508aa504ce70e908053ad1a47f2434f38
Yael Ejgenberg <yaelnegje@yahoo.com>
2012-02-12 16:38:30 +0000

    Updated throws statement.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@958 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/encryption/AsymmetricEnc.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/encryption/AsymmetricEnc.java
@@ -3,12 +3,14 @@
  */
 package edu.biu.scapi.midLayer.asymmetricCrypto.encryption;
 
+import java.io.IOException;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.spec.AlgorithmParameterSpec;
 
+import edu.biu.scapi.exceptions.UnInitializedException;
 import edu.biu.scapi.midLayer.asymmetricCrypto.keys.AsymKeyGenParameterSpec;
 import edu.biu.scapi.midLayer.ciphertext.Ciphertext;
 import edu.biu.scapi.midLayer.plaintext.Plaintext;
@@ -24,12 +26,12 @@ public interface AsymmetricEnc extends Cpa, Indistinguishable {
 	public void init(PublicKey publicKey, SecureRandom random);
 	public void init(PublicKey publicKey, PrivateKey privateKey);
 	public void init(PublicKey publicKey, PrivateKey privateKey, SecureRandom random);
-	public void init(PublicKey publicKey, PrivateKey privateKey, AlgorithmParameterSpec params, SecureRandom random);
-	public void init(PublicKey publicKey, PrivateKey privateKey, AlgorithmParameterSpec params);
+	public void init(PublicKey publicKey, PrivateKey privateKey, AlgorithmParameterSpec params, SecureRandom random) throws IllegalArgumentException, IOException;
+	public void init(PublicKey publicKey, PrivateKey privateKey, AlgorithmParameterSpec params) throws IllegalArgumentException, IOException;
 	public void init(PublicKey publicKey, AlgorithmParameterSpec params);
 	public void init(PublicKey publicKey, AlgorithmParameterSpec params, SecureRandom random);
-	public Ciphertext encrypt(Plaintext plaintext);
-	public Plaintext decrypt (Ciphertext ciphertext);
+	public Ciphertext encrypt(Plaintext plaintext) throws UnInitializedException;
+	public Plaintext decrypt (Ciphertext ciphertext)throws UnInitializedException;
 	public KeyPair generateKey(AsymKeyGenParameterSpec keyParams, SecureRandom random);
 	public KeyPair generateKey(SecureRandom random);
 	
