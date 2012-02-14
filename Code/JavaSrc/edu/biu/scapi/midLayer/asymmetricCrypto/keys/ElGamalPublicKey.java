4be7050f40f882678eef9d7a49ad7d6c66d3a673
Moriya Farbstein <moriyaw@gmail.com>
2012-02-14 08:12:58 +0000

    added getH function that should be in every implementation of ElGamal public key.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@980 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/keys/ElGamalPublicKey.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/keys/ElGamalPublicKey.java
@@ -2,6 +2,9 @@ package edu.biu.scapi.midLayer.asymmetricCrypto.keys;
 
 import java.security.PublicKey;
 
-public interface ElGamalPublicKey extends PublicKey {
+import edu.biu.scapi.primitives.dlog.GroupElement;
 
+public interface ElGamalPublicKey extends PublicKey {
+	
+	public GroupElement getH();
 }
