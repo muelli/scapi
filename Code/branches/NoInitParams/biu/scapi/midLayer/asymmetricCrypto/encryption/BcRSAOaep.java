6c37b9a44c117a73ee3a04914d26dd2030717767
Yael Ejgenberg <yaelnegje@yahoo.com>
2012-04-05 12:52:16 +0000

    1)Deleted all the init functions. From now on, all the objects in this layer are fully initialized upon construction. 2)Deleted all the static keyGen functions. Instead, each algorithm provides a generateKey without parameters and a generateKey with parameters. Only the relevant one is implementes in each algorithm. The other one throw UnsupportedOperationException. 3) Fixed the encryption and decryption functions so that they don't break up the message into blocks and send each block to BC RSAOaep for processing. Instead send the whole msg, BC should take care of the size limits of the input and throw necessary exceptions.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1051 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/branches/NoInitParams/biu/scapi/midLayer/asymmetricCrypto/encryption/BcRSAOaep.java
+++ Code/branches/NoInitParams/biu/scapi/midLayer/asymmetricCrypto/encryption/BcRSAOaep.java
@@ -1,6 +1,7 @@
 package edu.biu.scapi.midLayer.asymmetricCrypto.encryption;
 
 import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
@@ -19,6 +20,7 @@ import org.bouncycastle.crypto.encodings.OAEPEncoding;
 import org.bouncycastle.crypto.engines.RSABlindedEngine;
 
 import edu.biu.scapi.exceptions.FactoriesException;
+import edu.biu.scapi.exceptions.ScapiRuntimeException;
 import edu.biu.scapi.exceptions.UnInitializedException;
 import edu.biu.scapi.midLayer.ciphertext.BasicAsymCiphertext;
 import edu.biu.scapi.midLayer.ciphertext.Ciphertext;
@@ -35,165 +37,68 @@ import edu.biu.scapi.tools.Translation.BCParametersTranslator;
  */
 public class BcRSAOaep implements RSAOaep {
 	
-	private OAEPEncoding bcBlockCipher;				//the underling BC OAEP encoding
+	private OAEPEncoding bcBlockCipher;				//the underlying BC OAEP encoding
 	private CipherParameters privateParameters;		//parameters contains the private key and the random
 	private CipherParameters publicParameters;		//parameters contains the public key and the random
 	private boolean forEncryption = true;
 	private RSAPrivateKey privateKey;				
-	private RSAPublicKey publicKey;		
-	private AlgorithmParameterSpec params;			
+	private RSAPublicKey publicKey;					
 	private SecureRandom random;					//source of randomness
-	private boolean isInitialized = false;			//set to false until the init function is called
+	private boolean isKeySet;
+
 	
 	/**
-	 * Initialize this RSAOAEP encryption scheme with keys and AlgorithmParameterSpec.
-	 * After this initialization the user can encrypt and decrypt messages.
-	 * @param publicKey should be RSAPublicKey
-	 * @param privateKey should be RSAPrivateKey
-	 * @param params can be PAddingParameterSpec
+	 * Default constructor
 	 */
-	@Override
-	public void init(PublicKey publicKey, PrivateKey privateKey,
-			AlgorithmParameterSpec params) throws FactoriesException {
-		//call the corresponding init function with default source of randomness
-		init(publicKey, privateKey, params, new SecureRandom());
+	public BcRSAOaep(){
+		this(new SecureRandom());
 	}
-
+	
 	/**
-	 * Initialize this RSAOAEP encryption scheme with keys, AlgorithmParameterSpec and source of randomness.
-	 * After this initialization the user can encrypt and decrypt messages.
+	 * Constructor that lets the user choose the source of randomness
+	 * @param random
+	 */
+	public BcRSAOaep(SecureRandom random){
+		this.random = random;
+		//creates the OAEP encoding with RSABlindedEngine of BC
+		this.bcBlockCipher = new OAEPEncoding(new RSABlindedEngine());
+
+	}
+	/**
+	 * Set this RSAOAEP encryption scheme with a Public/Private key pair.
+	 * In thiscase the user can encrypt and decrypt messages.
 	 * @param publicKey should be RSAPublicKey
 	 * @param privateKey should be RSAPrivateKey
-	 * @param params can be PAddingParameterSpec
-	 * @param random source of secure randomness
 	 */
 	@Override
-	public void init(PublicKey publicKey, PrivateKey privateKey,
-			AlgorithmParameterSpec params, SecureRandom random) throws FactoriesException {
+	public void setKey(PublicKey publicKey, PrivateKey privateKey) {
 		//key should be RSA keys
-		if(!(publicKey instanceof RSAPublicKey) || !(privateKey instanceof RSAPrivateKey)){
+		if(!(publicKey instanceof RSAPublicKey)){
 			throw new IllegalArgumentException("keys should be instances of RSA keys");
 		}
-		//set the parameters
-		this.publicKey = (RSAPublicKey) publicKey;
-		this.privateKey = (RSAPrivateKey) privateKey;
-		this.params = params;
-		this.random = random;
-		
-		//create BC objects and initialize them
-		initBCCipher();
-		
-		isInitialized = true; //mark this object as initialized
-	}
-
-	/**
-	 * Initialize this RSAOAEP encryption scheme with keys.
-	 * After this initialization the user can encrypt and decrypt messages.
-	 * @param publicKey should be RSAPublicKey
-	 * @param privateKey should be RSAPrivateKey
-	 */
-	@Override
-	public void init(PublicKey publicKey, PrivateKey privateKey) {
-		//call the corresponding init function with default source of randomness
-		init(publicKey, privateKey, new SecureRandom());
-	}
-
-	/**
-	 * Initialize this RSAOAEP encryption scheme with keys and source of randomness.
-	 * After this initialization the user can encrypt and decrypt messages.
-	 * @param publicKey should be RSAPublicKey
-	 * @param privateKey should be RSAPrivateKey
-	 * @param random source of secure randomness
-	 */
-	@Override
-	public void init(PublicKey publicKey, PrivateKey privateKey,
-			SecureRandom random) {
-		//keys should be RSA keys
-		if(!(publicKey instanceof RSAPublicKey) || !(privateKey instanceof RSAPrivateKey)){
-			throw new IllegalArgumentException("keys should be instances of RSA keys");
+		if(privateKey!= null && !(privateKey instanceof RSAPrivateKey)){
+				throw new IllegalArgumentException("keys should be instances of RSA keys");
 		}
 		//set the parameters
 		this.publicKey = (RSAPublicKey) publicKey;
-		this.privateKey = (RSAPrivateKey) privateKey;
-		this.random = random;
-		
+		this.privateKey = (RSAPrivateKey) privateKey; //What happens if null?
+				
 		//create BC objects and initialize them
 		initBCCipher();
-		
-		isInitialized = true; //mark this object as initialized
+		isKeySet = true;
 	}
 
-	/**
-	 * Initialize this RSAOAEP encryption scheme with public key and AlgorithmParameterSpec.
-	 * After this initialization the user can encrypt messages but can not decrypt messages.
-	 * @param publicKey should be RSAPublicKey
-	 * @param params can be PaddingParameterSpec
-	 */
-	@Override
-	public void init(PublicKey publicKey, AlgorithmParameterSpec params) throws FactoriesException {
-		//call the corresponding init function with default source of randomness
-		init(publicKey, params, new SecureRandom());
-	}
+
 
 	/**
-	 * Initialize this RSAOAEP encryption scheme with public key, AlgorithmParameterSpec and source of randomness.
-	 * After this initialization the user can encrypt messages but can not decrypt messages.
-	 * @param publicKey should be RSAPublicKey
-	 * @param params can be PaddingParameterSpec
-	 * @param random source of secure randomness
-	 */
-	@Override
-	public void init(PublicKey publicKey, AlgorithmParameterSpec params,
-			SecureRandom random) throws FactoriesException {
-		//key should be RSA key
-		if(!(publicKey instanceof RSAPublicKey)){
-			throw new IllegalArgumentException("key should be instances of RSA key");
-		}
-		//set the parameters
-		this.publicKey = (RSAPublicKey) publicKey;
-		this.params = params;
-		this.random = random;
-		
-		//create BC objects and initialize them
-		initBCCipher();
-		
-		isInitialized = true; //mark this object as initialized
-	}
-
-	/**
-	 * Initialize this RSAOAEP encryption scheme with public key.
-	 * After this initialization the user can encrypt messages but can not decrypt messages.
+	 * Set this RSAOAEP encryption scheme only with public key.
+	 * In this case the user can encrypt messages but can not decrypt messages.
 	 * @param publicKey should be RSAPublicKey
 	 */
 	@Override
-	public void init(PublicKey publicKey) {
-		//call the corresponding init function with default source of randomness
-		init(publicKey, new SecureRandom());
-
+	public void setKey(PublicKey publicKey) {
+		setKey(publicKey, null);
 	}
-
-	/**
-	 * Initialize this RSAOAEP encryption scheme with public key and source of randomness.
-	 * After this initialization the user can encrypt messages but can not decrypt messages.
-	 * @param publicKey should be RSAPublicKey
-	 * @param random source of secure randomness
-	 */
-	@Override
-	public void init(PublicKey publicKey, SecureRandom random) {
-		//key should be RSA key
-		if(!(publicKey instanceof RSAPublicKey)){
-			throw new IllegalArgumentException("key should be instances of RSA key");
-		}
-		//set the parameters
-		this.publicKey = (RSAPublicKey) publicKey;
-		this.random = random;
-		
-		//create BC objects and initialize them
-		initBCCipher();
-		
-		isInitialized = true; //mark this object as initialized
-	}
-
 	/**
 	 * Creates BC OAEPEncoding with BC RSABlindedEngine, translate the keys and random to BC CipherParameters
 	 * and initialize BC object in encrypt mode.
@@ -201,8 +106,6 @@ public class BcRSAOaep implements RSAOaep {
 	 */
 	private void initBCCipher(){
 		
-		//creates the OAEP encoding with RSABlindedEngine of BC
-		bcBlockCipher = new OAEPEncoding(new RSABlindedEngine());
 		//translate the keys and random to BC parameters
 		privateParameters = BCParametersTranslator.getInstance().translateParameter(privateKey, random);
 		publicParameters = BCParametersTranslator.getInstance().translateParameter(publicKey, random);
@@ -211,18 +114,8 @@ public class BcRSAOaep implements RSAOaep {
 	}
 	
 	@Override
-	public boolean isInitialized() {
-		return isInitialized;
-	}
-
-	@Override
-	public AlgorithmParameterSpec getParams() throws UnInitializedException {
-		//if this object is not initialized, throw exception
-		if (!isInitialized()){
-			throw new UnInitializedException();
-		}
-		//return the params
-		return params;
+	public boolean isKeySet() {
+		return isKeySet;
 	}
 
 	/**
@@ -230,7 +123,7 @@ public class BcRSAOaep implements RSAOaep {
 	 */
 	@Override
 	public String getAlgorithmName() {
-		return "RSAOAEP";
+		return "RSA/OAEP";
 	}
 
 	/**
@@ -239,13 +132,9 @@ public class BcRSAOaep implements RSAOaep {
 	 * @return Ciphertext contains the encrypted plaintext
 	 */
 	@Override
-	public Ciphertext encrypt(Plaintext plaintext)
-			throws UnInitializedException {
-		//if the object is not initialized, throw UnInitialized exception
-		if (!isInitialized()){
-			throw new UnInitializedException();
-		}
+	public Ciphertext encrypt(Plaintext plaintext) {		
 		//if the underlying BC object used to the encryption is in decrypt mode - change it
+		//TODO make sure that this mechanism covers all possibilities.
 		if (!forEncryption){
 			forEncryption = true;
 			bcBlockCipher.init(forEncryption, publicParameters);
@@ -255,46 +144,17 @@ public class BcRSAOaep implements RSAOaep {
 		int inputBlockSize = bcBlockCipher.getInputBlockSize();
 		
 		//calculate the number of whole blocks
-		int numberBlocksInCipher = plaintextBytes.length / inputBlockSize;
-		int remainder = plaintextBytes.length % inputBlockSize;
+		//int numberBlocksInCipher = plaintextBytes.length / inputBlockSize;
+		//int remainder = plaintextBytes.length % inputBlockSize;
 		
-		byte[] ciphertext = new byte[0]; //prepare the ciphertext array
-		byte[] appendedCiphertext = new byte[0]; //in every round this array will hold the output of the previous rounds
-		
-		//encrypt every block in the plaintext and put the result in the ciphertext
-		for (int i=0; i<numberBlocksInCipher; i++){
-			try {
-				//encrypt block using BC OAEP object
-				byte[] outputBlock = bcBlockCipher.encodeBlock(plaintextBytes, i*inputBlockSize, inputBlockSize);
-				//because there is no fixed output length for all the inputs, we can not allocate the space before we get the output.
-				//therefore, in every loop we need to allocate a new space for this round output and the previous rounds output
-				ciphertext = new byte[appendedCiphertext.length + outputBlock.length];
-				
-				//copy the previous rounds output to the ciphertext
-				System.arraycopy(appendedCiphertext, 0, ciphertext, 0, appendedCiphertext.length);
-				//copy the encrypted block of this round to the ciphertext
-				System.arraycopy(outputBlock, 0, ciphertext, appendedCiphertext.length, outputBlock.length);
-				
-			} catch (InvalidCipherTextException e) {
-				e.printStackTrace();
-			}
+		byte[] ciphertext;
+		try {
+			//encrypt block using BC OAEP object
+			ciphertext = bcBlockCipher.encodeBlock(plaintextBytes, 0, plaintextBytes.length);
+		} catch (InvalidCipherTextException e) {
+			throw new ScapiRuntimeException(e.getMessage());
 		}
-		//if the last block is not in block size, it needs a special treatment
-		if (remainder != 0){
-			try {
-				byte[] outputBlock = bcBlockCipher.encodeBlock(plaintextBytes, numberBlocksInCipher*inputBlockSize, remainder);
-				//because there is no fixed output length for all the inputs, we can not allocate the space before we get the output.
-				//therefore, in every loop we need to allocate a new space for this round output and the previous rounds output
-				ciphertext = new byte[appendedCiphertext.length + outputBlock.length];
-				//copy the previous rounds output to the ciphertext
-				System.arraycopy(appendedCiphertext, 0, ciphertext, 0, appendedCiphertext.length);
-				//copy the encrypted block of this round to the ciphertext
-				System.arraycopy(outputBlock, 0, ciphertext, appendedCiphertext.length, outputBlock.length);
-			} catch (InvalidCipherTextException e) {
-				e.printStackTrace();
-			}
-		}
-		
+
 		//return a ciphertext with the encrypted plaintext
 		return new BasicAsymCiphertext(ciphertext);
 	}
@@ -303,67 +163,33 @@ public class BcRSAOaep implements RSAOaep {
 	 * Decrypts the given ciphertext according to the RSAOAEP algorithm using BC OAEPEncoding.
 	 * @param cipher the ciphertext to decrypt
 	 * @return Plaintext contains the decrypted ciphertext
-	 * @throws UnInitializedException 
+	 * @throws KeyException 
 	 */
 	@Override
-	public Plaintext decrypt(Ciphertext cipher) throws UnInitializedException {
+	public Plaintext decrypt(Ciphertext cipher) throws KeyException {
 		//if there is no private key can not decrypt, throw exception
 		if (privateKey == null){
-			throw new UnInitializedException("in order to decrypt a message, this object must be initialized with private key");
+			throw new KeyException("in order to decrypt a message, this object must be initialized with private key");
 		}
 		//cipher must be of type BasicAsymCiphertext
 		if (!(cipher instanceof BasicAsymCiphertext)){
 			throw new IllegalArgumentException("The ciphertext has to be of type BasicAsymCiphertext");
 		}
 		//if the underlying BC object used to the decryption is in encrypt mode - change it
+		//TODO make sure that this mechanism covers all possibilities.
 		if (forEncryption){
 			forEncryption = false;
 			bcBlockCipher.init(forEncryption, privateParameters);
 		}
 		
 		byte[] ciphertext = ((BasicAsymCiphertext) cipher).getBytes();
-		int outputBlockSize = bcBlockCipher.getInputBlockSize();
-		
-		//calculate the number of whole blocks
-		int numberBlocksInCipher = ciphertext.length / outputBlockSize;
-		int remainder = ciphertext.length % outputBlockSize;
-		
-		byte[] plaintext = new byte[0]; //prepare the plaintext array
-		byte[] appendedPlaintext = new byte[0]; //in every round this array will hold the output of the previous rounds
-		
-		//decrypt every block in the ciphertext and put the result in the paddedPlaintext
-		for (int i=0; i<numberBlocksInCipher; i++){
-			try {
-				//decrypt block using BC OAEP object
-				byte[] plaintextBlock = bcBlockCipher.decodeBlock(ciphertext, i*outputBlockSize, outputBlockSize);
-				//because there is no fixed output length for all the inputs, we can not allocate the space before we get the output.
-				//therefore, in every loop we need to allocate a new space for this round output and the previous rounds output
-				plaintext = new byte[appendedPlaintext.length + plaintextBlock.length];
-				//copy the previous rounds output to the ciphertext
-				System.arraycopy(appendedPlaintext, 0, plaintext, 0, appendedPlaintext.length);
-				//copy the encrypted block of this round to the ciphertext
-				System.arraycopy(plaintextBlock, 0, plaintext, appendedPlaintext.length, plaintextBlock.length);
-			} catch (InvalidCipherTextException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		//if the last block is not in block size, it needs a special treatment
-		if (remainder != 0){
-			try {
-				//decrypt block using BC OAEP object
-				byte[] plaintextBlock = bcBlockCipher.decodeBlock(ciphertext, numberBlocksInCipher*outputBlockSize, outputBlockSize);
-				//because there is no fixed output length for all the inputs, we can not allocate the space before we get the output.
-				//therefore, in every loop we need to allocate a new space for this round output and the previous rounds output
-				plaintext = new byte[appendedPlaintext.length + plaintextBlock.length];
-				
-				//copy the previous rounds output to the ciphertext
-				System.arraycopy(appendedPlaintext, 0, plaintext, 0, appendedPlaintext.length);
-				//copy the encrypted block of this round to the ciphertext
-				System.arraycopy(plaintextBlock, 0, plaintext, appendedPlaintext.length, plaintextBlock.length);
-			} catch (InvalidCipherTextException e) {
-				e.printStackTrace();
-			}
+
+		byte[] plaintext;
+		try {
+			//decrypt block using BC OAEP object
+			plaintext = bcBlockCipher.decodeBlock(ciphertext, 0, ciphertext.length);
+		} catch (InvalidCipherTextException e) {
+			throw new ScapiRuntimeException(e.getMessage());
 		}
 		//return a plaintext with the decrypted ciphertext
 		return new BasicPlaintext(plaintext);
@@ -376,43 +202,18 @@ public class BcRSAOaep implements RSAOaep {
 	 * @throws InvalidParameterSpecException if keyParams is not instance of RSAPssParameterSpec
 	 */
 	@Override
-	public KeyPair generateKey(AlgorithmParameterSpec keyParams) throws InvalidParameterSpecException {
-		//call the static function that generates RSA key pair
-		return keyGen(keyParams);
-		
+	public KeyPair generateKey() {
+		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * Generates a KeyPair contains set of RSAPublicKEy and RSAPrivateKey using the given source of randomness.
-	 * @param keyParams RSAPssParameterSpec
-	 * @return KeyPair contains keys for this RSAPss object
-	 * @throws InvalidParameterSpecException if keyParams is not instance of RSAPssParameterSpec
-	 */
-	@Override
-	public KeyPair generateKey(AlgorithmParameterSpec keyParams,
-			SecureRandom random) throws InvalidParameterSpecException  {
-		//call the static function that generates RSA key pair
-		return keyGen(keyParams, random);
-	}
 	
 	/**
-	 * Static function that generates RSA key pair using default source of randomness.
+	 * Generate an RSA key pair using the given source of randomness.
 	 * @param keyParams RSAPssParameterSpec
 	 * @return KeyPair contains keys for this RSAPss object
 	 * @throws InvalidParameterSpecException if keyParams is not instance of RSAPssParameterSpec
 	 */
-	public static KeyPair keyGen(AlgorithmParameterSpec keyParams) throws InvalidParameterSpecException {
-		//call the static keyGen function with default source of randomness
-		return keyGen(keyParams, new SecureRandom());
-	}
-	
-	/**
-	 * Static function that generates RSA key pair using the given source of randomness.
-	 * @param keyParams RSAPssParameterSpec
-	 * @return KeyPair contains keys for this RSAPss object
-	 * @throws InvalidParameterSpecException if keyParams is not instance of RSAPssParameterSpec
-	 */
-	public static KeyPair keyGen(AlgorithmParameterSpec keyParams, SecureRandom random) throws InvalidParameterSpecException {
+	public KeyPair generateKey(AlgorithmParameterSpec keyParams) throws InvalidParameterSpecException {
 		//if keyParams is not the expected, throw exception
 		if (!(keyParams instanceof RSAKeyGenParameterSpec)){
 			throw new InvalidParameterSpecException("keyParams should be instance of RSAKeyGenParameterSpec");
