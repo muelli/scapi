9f9d7ca23aa2319a9a9bbb5cca96615774a26f2b
Yael Ejgenberg <yaelnegje@yahoo.com>
2012-04-05 12:49:56 +0000

    Updated the Security level from CCA to CCA2 and NON-MALLEABLE.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1050 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/branches/NoInitParams/biu/scapi/midLayer/asymmetricCrypto/encryption/RSAOaep.java
+++ Code/branches/NoInitParams/biu/scapi/midLayer/asymmetricCrypto/encryption/RSAOaep.java
@@ -1,6 +1,7 @@
 package edu.biu.scapi.midLayer.asymmetricCrypto.encryption;
 
-import edu.biu.scapi.securityLevel.Cca;
+import edu.biu.scapi.securityLevel.Cca2;
+import edu.biu.scapi.securityLevel.NonMalleable;
 
 /**
  * General interface for RSA OAEP encryption scheme. Every concrete implementation of RSA OAEP encryption should implement this interface.
@@ -9,6 +10,6 @@ import edu.biu.scapi.securityLevel.Cca;
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
-public interface RSAOaep extends AsymmetricEnc, Cca{
+public interface RSAOaep extends AsymmetricEnc, Cca2, NonMalleable{
 
 }
