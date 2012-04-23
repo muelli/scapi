8e765a7b1ae2542e4fa3ca430298c64db1cbba41
Moriya Farbstein <moriyaw@gmail.com>
2012-04-23 08:55:48 +0000

    1. deleted init and isInitialized functions. 2. deleted all throws of UnInitialized exceptions.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1112 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/branches/NoInitParams/biu/scapi/primitives/kdf/KeyDerivationFunction.java
+++ Code/branches/NoInitParams/biu/scapi/primitives/kdf/KeyDerivationFunction.java
@@ -1,11 +1,6 @@
 package edu.biu.scapi.primitives.kdf;
 
-import java.security.InvalidKeyException;
-import java.security.spec.AlgorithmParameterSpec;
-
 import javax.crypto.SecretKey;
-
-import edu.biu.scapi.exceptions.UnInitializedException;
 
 /** 
  * General interface of key derivation function. Every class in this family should implement this interface. <p>
@@ -16,45 +11,22 @@ import edu.biu.scapi.exceptions.UnInitializedException;
  */
 public interface KeyDerivationFunction {
 	
-	/**
-	 * Initializes this kdf with the secret key. The initialization is for the underlying object the KDF used.
-	 * @param secretKey the secret key
-	 * @throws InvalidKeyException 
-	 *  */
-	public void init(SecretKey secretKey) throws InvalidKeyException;
-
-	/** 
-	 * Initializes this kdf with the secret key and the auxiliary parameters. The initialization is for the underlying object the KDF used.
-	 * @param secretKey secret key
-	 * @param params algorithm parameters
-	 * @throws InvalidKeyException 
-	 */
-	public void init(SecretKey secretKey, AlgorithmParameterSpec params) throws InvalidKeyException;
-	
-	/**
-	 * An object trying to use an instance of kdf needs to check if it has already been initialized.
-	 * @return true if the object was initialized by calling the function init.
-	 */
-	public boolean isInitialized();
-	
 	/** 
 	 * Generates a new secret key from the given seed and IV.
 	 * @param seedForGeneration the secret key that is the seed for the key generation
 	 * @param len the required output key length
 	 * @param iv info for the key generation
 	 * @return secret key the generated key
-	 * @throws UnInitializedException if this object is not initialized
 	 */
-	public SecretKey generateKey(SecretKey seedForGeneration, int outLen,  byte[] iv) throws UnInitializedException;
+	public SecretKey generateKey(SecretKey seedForGeneration, int outLen,  byte[] iv) ;
 	
 	/** 
 	 * Generates a new secret key from the given seed.
 	 * @param seedForGeneration the secret key that is the seed for the key generation
 	 * @param len the required output key length
 	 * @return secret key the generated key
-	 * @throws UnInitializedException if this object is not initialized
 	 */
-	public SecretKey generateKey(SecretKey seedForGeneration, int outLen) throws UnInitializedException;
+	public SecretKey generateKey(SecretKey seedForGeneration, int outLen) ;
 	
 	/** 
 	 * Generates a new secret key from the given seed.
@@ -64,9 +36,8 @@ public interface KeyDerivationFunction {
 	 * @param outKey the array to put the generated key bytes
 	 * @param outoff the offset within the output array to put the generated key bytes from
 	 * @param outlen the required output key length
-	 * @throws UnInitializedException if this object is not initialized
 	 */
-	public void generateKey(byte[] seedForGeneration, int inOff, int inLen, byte[] outKey, int outOff, int outLen) throws UnInitializedException;
+	public void generateKey(byte[] seedForGeneration, int inOff, int inLen, byte[] outKey, int outOff, int outLen) ;
 	
 	/** 
 	 * Generates a new secret key from the given seed and iv.
@@ -77,7 +48,6 @@ public interface KeyDerivationFunction {
 	 * @param outoff the offset within the output array to put the generated key bytes from
 	 * @param outlen the required output key length
 	 * @param iv info for the key generation
-	 * @throws UnInitializedException if this object is not initialized
 	 */
-	public void generateKey(byte[] seedForGeneration, int inOff, int inLen, byte[] outKey, int outOff, int outLen, byte[] iv) throws UnInitializedException;
+	public void generateKey(byte[] seedForGeneration, int inOff, int inLen, byte[] outKey, int outOff, int outLen, byte[] iv) ;
 }
