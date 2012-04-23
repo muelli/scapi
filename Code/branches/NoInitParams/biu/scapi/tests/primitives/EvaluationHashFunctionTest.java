fe4c1d9b77dd52620df5e4ccfc797fa1ce17ae2a
Moriya Farbstein <moriyaw@gmail.com>
2012-04-23 09:14:35 +0000

    remove padding from the testData since the padding initialization moved to the constructor and is no longer in the init function
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1127 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/branches/NoInitParams/biu/scapi/tests/primitives/EvaluationHashFunctionTest.java
+++ Code/branches/NoInitParams/biu/scapi/tests/primitives/EvaluationHashFunctionTest.java
@@ -1,6 +1,5 @@
 package edu.biu.scapi.tests.primitives;
 
-import edu.biu.scapi.paddings.PaddingParameterSpec;
 import edu.biu.scapi.primitives.universalHash.UniversalHash;
 
 /**
@@ -27,8 +26,8 @@ public class EvaluationHashFunctionTest extends UniversalHashTest {
 		byte[] output = {0, 1, 1, 1, 1, 0, 0, 0};//output - the related polynomial is f(x) = x^8 + x^16 + x^32  
 		byte[] key = {0, 1, 0, 0, 0, 0, 0, 0};//key - the related polynomial is f(x) = x^8
 		
-		PaddingParameterSpec padding = new PaddingParameterSpec("NoPadding");
-		addData(input ,output ,key, padding);
+		
+		addData(input ,output ,key);
 		
 		//byte[] inputToPad = {1, 0, 1, 0, 0, 0, 0, 0, 	  1, 0};
 		//byte[] outputAfterPad = {0, 1, 1, 1, 1, 0, 0, 0};//output - the related polynomial is f(x) = x^8 + x^16 + x^32  
@@ -41,14 +40,14 @@ public class EvaluationHashFunctionTest extends UniversalHashTest {
 		byte[] output1 = {0, 0, 0, 0, 0, 0, 0, 0};//output - the related polynomial is f(x) = 1
 		byte[] key1 = {1, 0, 0, 0, 0, 0, 0, 0};//key - the related polynomial is f(x) = 1
 		
-		addData( input1 ,output1 ,key1, padding);
+		addData( input1 ,output1 ,key1);
 		
 		byte[] input2 = {1, 3, 1, 0, 0, 0, 0, 0, 	0, 0, 0, 0, 0, 0, 0, 0,      1, 0, 1 ,0 ,0, 0, 0, 0};//input - the related polynomial is 
 																										 //P(y) = (1 + x^8 + x^9 + x^16) + (1+x^16)y^2 + y^3
 		byte[] output2 = {12, 1, 12, 5, 3, 1, 0, 0};//output - the related polynomial is f(x) = 1 + x^2 + x^3 + x^4 + x^8 + x^18 + x^19 + x^24 + x^26 + x^33 + x^40 
 		byte[] key2 = {3, 1, 0, 0, 0, 0, 0, 0};//key - the related polynomial is f(x) = 1 + x + x^8
 		
-		addData( input2 ,output2 ,key2, padding);
+		addData( input2 ,output2 ,key2);
 		
 		
 	}
