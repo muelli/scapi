31a0d30ad30d9f07c0536900d52b5e0c2e07b346
Moriya Farbstein <moriyaw@gmail.com>
2012-04-23 07:24:18 +0000

    1. added constructors that receive random. 2. change init function to setKey function.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1093 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/branches/NoInitParams/biu/scapi/primitives/prf/bc/BcTripleDES.java
+++ Code/branches/NoInitParams/biu/scapi/primitives/prf/bc/BcTripleDES.java
@@ -1,7 +1,8 @@
 package edu.biu.scapi.primitives.prf.bc;
 
 import java.security.InvalidKeyException;
-import java.security.spec.AlgorithmParameterSpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 import javax.crypto.SecretKey;
 
@@ -25,26 +26,37 @@ public final class BcTripleDES extends BcPRP implements TripleDES{
 	}
 	
 	/**
+	 * Receives random object to use.
+	 * Passes it and the DesedeEngine of BC to the abstract super class.
+	 * @param random SecureRandom to use
+	 */
+	public BcTripleDES(SecureRandom random) {
+		
+		super(new DESedeEngine(), random);
+	}
+	
+	/**
+	 * Receives name of random algorithm to use.
+	 * Passes it and the DesedeEngine of BC to the abstract super class.
+	 * @param randNumGenAlg random algorithm to use
+	 */
+	public BcTripleDES(String randNumGenAlg) throws NoSuchAlgorithmException {
+		
+		super(new DESedeEngine(), SecureRandom.getInstance(randNumGenAlg));
+	}
+	
+	/**
 	 * initializes this Triple-DES with secret key.
 	 * @param secretKey the secret key
 	 * @throws InvalidKeyException 
 	 */
-	public void init(SecretKey secretKey) throws InvalidKeyException {
+	public void setKey(SecretKey secretKey) throws InvalidKeyException {
 		int len = secretKey.getEncoded().length;
 		//TripleDes key size should be 128/192 bits 
 		if(len!=16 && len!=24){
 			throw new InvalidKeyException("TripleDes key size should be 128/192 bits long");
 		}
-		super.init(secretKey);
-	}
-	
-	public void init(SecretKey secretKey, AlgorithmParameterSpec params) throws InvalidKeyException {
-		int len = secretKey.getEncoded().length;
-		//TripleDes key size should be 128/192 bits 
-		if(len!=16 && len!=24){
-			throw new InvalidKeyException("TripleDes key size should be 128/192 bits long");
-		}
-		super.init(secretKey, params);
+		super.setKey(secretKey);
 	}
 
 }
