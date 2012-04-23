31fa5433abeb2a5856d7b9cab5db7431bc3ec1da
Moriya Farbstein <moriyaw@gmail.com>
2012-04-23 08:34:09 +0000

    added default constructor and constructor that receive curve and file
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1109 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/branches/NoInitParams/biu/scapi/primitives/dlog/miracl/MiraclAdapterDlogEC.java
+++ Code/branches/NoInitParams/biu/scapi/primitives/dlog/miracl/MiraclAdapterDlogEC.java
@@ -2,12 +2,20 @@ package edu.biu.scapi.primitives.dlog.miracl;
 
 
 
+import java.io.IOException;
+
 import edu.biu.scapi.primitives.dlog.DlogEllipticCurve;
 import edu.biu.scapi.primitives.dlog.DlogGroupEC;
 
 public abstract class MiraclAdapterDlogEC extends DlogGroupEC 
 										  implements DlogEllipticCurve{
 	
+	protected MiraclAdapterDlogEC(){}
+	
+	public MiraclAdapterDlogEC(String fileName, String curveName) throws IOException {
+		super(fileName, curveName);
+	}
+
 	private native long createMip();
 
 	protected long mip = 0; ///MIRACL pointer
