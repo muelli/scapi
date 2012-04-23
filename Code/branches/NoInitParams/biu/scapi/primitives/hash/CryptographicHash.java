d34ce2e8af3f096baeb4823137db12c2ef775e79
Moriya Farbstein <moriyaw@gmail.com>
2012-04-23 06:44:14 +0000

    1. deleted isInitialized function. 2. delete all throws of UnInitialized exception
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1074 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/branches/NoInitParams/biu/scapi/primitives/hash/CryptographicHash.java
+++ Code/branches/NoInitParams/biu/scapi/primitives/hash/CryptographicHash.java
@@ -1,6 +1,5 @@
 package edu.biu.scapi.primitives.hash;
 
-import edu.biu.scapi.exceptions.UnInitializedException;
 
 /**
  * General interface for CryptographicHash. Every concrete class should implement this interface. <p>
@@ -11,13 +10,6 @@ import edu.biu.scapi.exceptions.UnInitializedException;
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
  */
 public interface CryptographicHash {
-	
-	/**
-	 * An object trying to use an instance of CryptographicHash needs to check if it has already been initialized.
-	 * Usually this means that the function init was called, CryptographicHash has no init function so it always return true.
-	 * @return true if the object was initialized. 
-	 */
-	public boolean isInitialized();
 
 	/** 
 	 * @return The algorithm name. For example - SHA1
@@ -34,15 +26,13 @@ public interface CryptographicHash {
 	 * @param in input byte array
 	 * @param inOffset the offset within the byte array
 	 * @param inLen the length. The number of bytes to take after the offset
-	 * @throws UnInitializedException if this object is not initialized
 	 * */
-	public void update(byte[] in, int inOffset, int inLen) throws UnInitializedException;
+	public void update(byte[] in, int inOffset, int inLen);
 
 	/** 
 	 * Completes the hash computation and puts the result in the out array.
 	 * @param out the output in byte array
 	 * @param outOffset the offset which to put the result bytes from
-	 * @throws UnInitializedException if this object is not initialized
 	 */
-	public void hashFinal(byte[] out, int outOffset) throws UnInitializedException;
+	public void hashFinal(byte[] out, int outOffset);
 }
