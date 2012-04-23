02a122718c3bee0ffc34344af6f7838466b4f908
Moriya Farbstein <moriyaw@gmail.com>
2012-04-23 06:37:52 +0000

    according our new approach: 1. deleted init functions. 2. added setKey and isKeySet functions 3. added generateKey functions 4. deleted all UnInitialized exceptions
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1071 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/branches/NoInitParams/biu/scapi/primitives/prg/PseudorandomGenerator.java
+++ Code/branches/NoInitParams/biu/scapi/primitives/prg/PseudorandomGenerator.java
@@ -1,10 +1,10 @@
 package edu.biu.scapi.primitives.prg;
 
+import java.security.InvalidKeyException;
 import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.InvalidParameterSpecException;
 
 import javax.crypto.SecretKey;
-
-import edu.biu.scapi.exceptions.UnInitializedException;
 
 /** 
  * General interface of pseudorandom generator. Every concrete class in this family should implement this interface. <p>
@@ -16,44 +16,50 @@ import edu.biu.scapi.exceptions.UnInitializedException;
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
   */
 public interface PseudorandomGenerator {
-	/** 
-	 * Initializes this prg with the secret key
-	 * @param secretKey - the secret key
-	 */
-	public void init(SecretKey secretKey);
-
-	/** 
-	 * Initializes this prg with the secret key and the auxiliary parameters
+	
+	/**
+	 * Sets the secret key for this prg.
+	 * The key can be changed at any time. 
 	 * @param secretKey secret key
-	 * @param params the algorithm auxiliary parameters
+	 * @throws InvalidKeyException 
 	 */
-	public void init(SecretKey secretKey, AlgorithmParameterSpec params);
-
+	public void setKey(SecretKey secretKey) throws InvalidKeyException;
+	
 	/**
 	 * An object trying to use an instance of prg needs to check if it has already been initialized.
-	 * @return true if the object was initialized by calling the function init.
+	 * @return true if the object was initialized by calling the function setKey.
 	 */
-	public boolean isInitialized();
-	
-	/** 
-	 * @return the parameter spec of this PRG
-	 * @throws UnInitializedException if this object is not initialized
-	 */
-	public AlgorithmParameterSpec getParams() throws UnInitializedException;
+	public boolean isKeySet();
 
 	/** 
 	 * @return the algorithm name. For example - RC4
 	 */
 	public String getAlgorithmName();
+	
+	/**
+	 * Generates a secret key to initialize this prg object.
+	 * @param keySize algorithmParameterSpec contains the required parameters for the key generation
+	 * @return the generated secret key
+	 * @throws InvalidParameterSpecException 
+	 */
+	public SecretKey generateKey(AlgorithmParameterSpec keyParams) throws InvalidParameterSpecException;
+	
+	/**
+	 * Generates a secret key to initialize this prg object.
+	 * @param keySize is the required secret key size in bits 
+	 * @return the generated secret key 
+	 */
+	public SecretKey generateKey(int keySize);
 
 	/**
 	 * Streams the prg bytes.
 	 * @param outBytes - output bytes. The result of streaming the bytes.
 	 * @param outOffset - output offset
 	 * @param outlen - the required output length
-	 * @throws UnInitializedException if this object is not initialized
 	 */
-	public void getPRGBytes(byte[] outBytes, int outOffset, int outlen) throws UnInitializedException;
+	public void getPRGBytes(byte[] outBytes, int outOffset, int outlen) ;
+	
+	
 
 	
 }
