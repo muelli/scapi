ff5be7bc95389d295811c95d0bf2fcfd688ede8a
Yael Ejgenberg <yaelnegje@yahoo.com>
2012-04-25 10:39:41 +0000

    Moved common code to RSAOaepAbs.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1138 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/branches/NoInitParams/biu/scapi/midLayer/asymmetricCrypto/encryption/BcRSAOaep.java
+++ Code/branches/NoInitParams/biu/scapi/midLayer/asymmetricCrypto/encryption/BcRSAOaep.java
@@ -35,7 +35,7 @@ import edu.biu.scapi.tools.Translation.BCParametersTranslator;
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
-public class BcRSAOaep implements RSAOaep {
+public class BcRSAOaep extends RSAOaepAbs {
 	
 	private OAEPEncoding bcBlockCipher;				//the underlying BC OAEP encoding
 	private CipherParameters privateParameters;		//parameters contains the private key and the random
@@ -43,8 +43,7 @@ public class BcRSAOaep implements RSAOaep {
 	private boolean forEncryption = true;
 	private RSAPrivateKey privateKey;				
 	private RSAPublicKey publicKey;					
-	private SecureRandom random;					//source of randomness
-	private boolean isKeySet;
+	
 
 	
 	/**
@@ -113,19 +112,7 @@ public class BcRSAOaep implements RSAOaep {
 		bcBlockCipher.init(forEncryption, publicParameters);
 	}
 	
-	@Override
-	public boolean isKeySet() {
-		return isKeySet;
-	}
-
-	/**
-	 * @return the name of this Asymmetric encryption - "RSAOAEP"
-	 */
-	@Override
-	public String getAlgorithmName() {
-		return "RSA/OAEP";
-	}
-
+	
 	/**
 	 * Encrypts the given plaintext according to the RSAOAEP algorithm using BC OAEPEncoding.
 	 * @param plaintext the plaintext to encrypt
@@ -195,41 +182,4 @@ public class BcRSAOaep implements RSAOaep {
 		return new BasicPlaintext(plaintext);
 	}
 	
-	/**
-	 * Generates a KeyPair contains set of RSAPublicKEy and RSAPrivateKey using default source of randomness.
-	 * @param keyParams RSAPssParameterSpec
-	 * @return KeyPair contains keys for this RSAPss object
-	 * @throws InvalidParameterSpecException if keyParams is not instance of RSAPssParameterSpec
-	 */
-	@Override
-	public KeyPair generateKey() {
-		throw new UnsupportedOperationException();
-	}
-
-	
-	/**
-	 * Generate an RSA key pair using the given source of randomness.
-	 * @param keyParams RSAPssParameterSpec
-	 * @return KeyPair contains keys for this RSAPss object
-	 * @throws InvalidParameterSpecException if keyParams is not instance of RSAPssParameterSpec
-	 */
-	public KeyPair generateKey(AlgorithmParameterSpec keyParams) throws InvalidParameterSpecException {
-		//if keyParams is not the expected, throw exception
-		if (!(keyParams instanceof RSAKeyGenParameterSpec)){
-			throw new InvalidParameterSpecException("keyParams should be instance of RSAKeyGenParameterSpec");
-		}
-		
-		try {
-			//generates keys using the KeyPairGenerator
-			KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-			generator.initialize(keyParams, random);
-			return generator.generateKeyPair(); 
-		} catch(InvalidAlgorithmParameterException e){
-			//shouldn't occur since the parameterSpec is valid for RSA
-		} catch (NoSuchAlgorithmException e) {
-			//shouldn't occur since RSA is a valid algorithm
-			e.printStackTrace();
-		}
-		return null;
-	}
 }
