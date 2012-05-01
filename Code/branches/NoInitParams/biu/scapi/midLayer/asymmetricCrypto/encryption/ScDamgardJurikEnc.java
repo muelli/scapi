b364e925ed05c13dfc772d953472fb88f93867b8
Yael Ejgenberg <yaelnegje@yahoo.com>
2012-05-01 12:45:12 +0000

    1)Implemented: decrypt add rerandomize multByConst 2) Added comments.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1183 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/branches/NoInitParams/biu/scapi/midLayer/asymmetricCrypto/encryption/ScDamgardJurikEnc.java
+++ Code/branches/NoInitParams/biu/scapi/midLayer/asymmetricCrypto/encryption/ScDamgardJurikEnc.java
@@ -12,17 +12,16 @@ import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.spec.AlgorithmParameterSpec;
 import java.security.spec.InvalidParameterSpecException;
-import java.security.spec.RSAKeyGenParameterSpec;
 import java.util.Vector;
 
 import org.bouncycastle.util.BigIntegers;
 
+import edu.biu.scapi.exceptions.ScapiRuntimeException;
 import edu.biu.scapi.midLayer.asymmetricCrypto.keys.DamgardJurikPrivateKey;
 import edu.biu.scapi.midLayer.asymmetricCrypto.keys.DamgardJurikPublicKey;
 import edu.biu.scapi.midLayer.asymmetricCrypto.keys.ScDamgardJurikPrivateKey;
 import edu.biu.scapi.midLayer.asymmetricCrypto.keys.ScDamgardJurikPublicKey;
 import edu.biu.scapi.midLayer.ciphertext.Ciphertext;
-import edu.biu.scapi.midLayer.ciphertext.CramerShoupCiphertext;
 import edu.biu.scapi.midLayer.ciphertext.DJCiphertext;
 import edu.biu.scapi.midLayer.plaintext.BasicPlaintext;
 import edu.biu.scapi.midLayer.plaintext.BigIntegerPlainText;
@@ -32,6 +31,8 @@ import edu.biu.scapi.primitives.trapdoorPermutation.ScRSAPermutation;
 import edu.biu.scapi.tools.math.MathAlgorithms;
 
 /**
+ * Damgard Jurik is an asymmetric encryption scheme based on the Paillier encryption scheme.
+ * This encryption scheme is CPA-secure and Indistinguishable.
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
  *
  */
@@ -40,23 +41,37 @@ public class ScDamgardJurikEnc implements DamgardJurikEnc {
 	private DamgardJurikPublicKey publicKey;
 	private DamgardJurikPrivateKey privateKey;
 	private SecureRandom random;
-	private BigInteger qMinusOne; 
 	private boolean isKeySet = false;
 
 
-	/* (non-Javadoc)
+	/**
+	 * Default constructor.
+	 */
+	public ScDamgardJurikEnc(){
+		random = new SecureRandom();
+	}
+	
+	/**
+	 * Constructor that lets the user choose the source of randomness
+	 * @param random
+	 */
+	public ScDamgardJurikEnc(SecureRandom rnd){
+		random = rnd;
+	}
+	/**
+	 * 
 	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#setKey(java.security.PublicKey, java.security.PrivateKey)
 	 */
 	@Override
 	public void setKey(PublicKey publicKey, PrivateKey privateKey)	throws InvalidKeyException {
-		//public key should be Cramer Shoup public key
+		//public key should be Damgard Jurik public key
 		if(!(publicKey instanceof ScDamgardJurikPublicKey)){
 			throw new InvalidKeyException("The public key must be of type DamgardJurikPublicKey");
 		}
 		//Set the public key
 		this.publicKey = (ScDamgardJurikPublicKey) publicKey;
 
-		//private key should be Cramer Shoup private key	
+		//private key should be Damgard Jurik private key or null if we are only setting the public key.	
 		if(privateKey == null){
 			//If the private key in the argument is null then this instance's private key should be null.  
 			this.privateKey = null;
@@ -71,7 +86,7 @@ public class ScDamgardJurikEnc implements DamgardJurikEnc {
 
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#setKey(java.security.PublicKey)
 	 */
 	@Override
@@ -80,7 +95,7 @@ public class ScDamgardJurikEnc implements DamgardJurikEnc {
 
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#isKeySet()
 	 */
 	@Override
@@ -89,7 +104,7 @@ public class ScDamgardJurikEnc implements DamgardJurikEnc {
 		return isKeySet;
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#getAlgorithmName()
 	 */
 	@Override
@@ -98,10 +113,46 @@ public class ScDamgardJurikEnc implements DamgardJurikEnc {
 		return "DamgardJurik";
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#encrypt(edu.biu.scapi.midLayer.plaintext.Plaintext)
+	/** 
+	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#generateKey(java.security.spec.AlgorithmParameterSpec)
+	 * @param keyParams has to be an instance of DJKeyGenParameterSpec
 	 */
 	@Override
+	public KeyPair generateKey(AlgorithmParameterSpec keyParams)throws InvalidParameterSpecException {
+		if(!(keyParams instanceof DJKeyGenParameterSpec)){
+			throw new InvalidParameterSpecException("keyParams has to be an instance of DJKeyGenParameterSpec");
+		}
+		
+		DJKeyGenParameterSpec params = (DJKeyGenParameterSpec)keyParams;
+		//Choose an RSA modulus n = p*q of length k bits.
+		//Compute t = lcmp(p-1, q-1) where lcm is the least common multiple and can be computed as lcm(a,b) = a*b/gcd(a,b)
+		//Let n be the Public Key
+		//Let t be the Private Key
+		RSAModulus rsaMod = ScRSAPermutation.generateRSAModulus(params.getModulusLength(), params.getCertainty(), random);
+		
+		BigInteger pMinus1 = rsaMod.p.subtract(BigInteger.ONE);
+		BigInteger qMinus1 = rsaMod.q.subtract(BigInteger.ONE);
+		BigInteger gcdPMinus1QMinus1 = pMinus1.gcd(qMinus1);
+		BigInteger t = (pMinus1.multiply(qMinus1)).divide(gcdPMinus1QMinus1);
+		BigInteger dForS1 = generateD(rsaMod.n, t); //precalculate d for the case that s == 1
+		
+		return new KeyPair(new ScDamgardJurikPublicKey(rsaMod.n), new ScDamgardJurikPrivateKey(t, dForS1));
+	}
+
+	/**
+	 * This function is not supported for this encryption scheme.
+	 * @return 
+	 * @throws UnsupportedOperationException
+	 */
+	public KeyPair generateKey() {
+		throw new UnsupportedOperationException("Use generateKey function with DJKeyGenParameterSpec");
+	}
+	
+	/** 
+	 * This function performs the encryption of he given plain text
+	 * @param plaintext can be an instance of BasicPlaintext or an instance of BigIntegerPlainText.
+	 * @return an object of type DJCiphertext holding the encryption of the plaintext.
+	 */
 	public Ciphertext encrypt(Plaintext plainText) {
 		BigInteger x; 
 		if(plainText instanceof BasicPlaintext){
@@ -128,11 +179,16 @@ public class ScDamgardJurikEnc implements DamgardJurikEnc {
 		BigInteger  mult1= (publicKey.getModulus().add(BigInteger.ONE)).modPow(x, Ntag);
 		BigInteger mult2 = r.modPow(N, Ntag);
 		BigInteger c = (mult1.multiply(mult2)).mod(Ntag);
+
+		//Wrap the BigInteger c with DJCiphertext and return it
 		return new DJCiphertext(c);
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#decrypt(edu.biu.scapi.midLayer.ciphertext.Ciphertext)
+	 * @param cipher has to be an instance of DJCiphertext
+	 * @throws KeyException if the Private Key has not been set for this object 
+	 * @throws IllegalArgumentException if cipher is not an instance of DJCiphertext
 	 */
 	@Override
 	public Plaintext decrypt(Ciphertext cipher) throws KeyException {
@@ -145,16 +201,15 @@ public class ScDamgardJurikEnc implements DamgardJurikEnc {
 			throw new IllegalArgumentException("cipher should be instance of DJCiphertext");
 		}
 		
-		//Plaintext plaintext = null;
-
 		DJCiphertext djCipher = (DJCiphertext) cipher;
 		//n is the modulus in the public key.
 		//Calculate s = (|cipher| -1) / |n|
 		int s = ((djCipher).getCipher().bitLength() - 1) / publicKey.getModulus().bitLength();
 
 		//Calculate N and N' based on s: N = n^s, N' = n^(s+1)
-		BigInteger N = publicKey.getModulus().pow(s);
-		BigInteger Ntag = publicKey.getModulus().pow(s+1);
+		BigInteger n = publicKey.getModulus();
+		BigInteger N = n.pow(s);
+		BigInteger Ntag = n.pow(s+1);
 		
 		//Make sure the cipher belongs to ZN'
 		if(djCipher.getCipher().compareTo(BigInteger.ZERO) < 0 || djCipher.getCipher().compareTo(Ntag) >= 0)
@@ -170,9 +225,6 @@ public class ScDamgardJurikEnc implements DamgardJurikEnc {
 			d = generateD(N, privateKey.getT());
 		}
 		
-		//Compute (cipher ^ d) mod N'
-		BigInteger cipherToD = djCipher.getCipher().modPow(d, Ntag);  
-	
 		//Compute x as the discrete logarithm of c^d to the base (1+n)modulo N'
 		//This is done by the following computation
 		//	a=c^d
@@ -191,75 +243,160 @@ public class ScDamgardJurikEnc implements DamgardJurikEnc {
 		//	end
 		//	OUTPUT x
 		
+		//Compute (cipher ^ d) mod N'
+		BigInteger a = djCipher.getCipher().modPow(d, Ntag);  
+		BigInteger x = BigInteger.ZERO;
+		BigInteger t1, t2;
+		for(int j = 1; j <= s; j++){
+			t1 = (a.mod(n.pow(j+1)).subtract(BigInteger.ONE)).divide(n);
+			t2 = x;
+			for(int k = 2; k <=j; k++){
+				x = x.subtract(BigInteger.ONE);
+				t2 = (t2.multiply(x)).mod(n.pow(j));
+				BigInteger factorialK = MathAlgorithms.factorialBI(k);
+				BigInteger temp = (t2.multiply(n.pow(k-1))).divide(factorialK);
+				t1 = t1.subtract(temp).mod(n.pow(j));
+			}
+			x = t1;
+		}
 		
-		
-		return null;
+		return new BigIntegerPlainText(x);
 	}
 
+
+	/**
+	 * This function takes an encryption of some plaintext (let's call it originalPlaintext) and returns a cipher that "looks" different but<p>
+	 * it is also an encryption of originalPlaintext.
+	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.DamgardJurikPaillierEnc#reRandomize(edu.biu.scapi.midLayer.ciphertext.Ciphertext)
+	 */
+	@Override
+	public Ciphertext reRandomize(Ciphertext cipher) {
+		//ciphertext should be Damgard-Jurik ciphertext
+		if (!(cipher instanceof DJCiphertext)){
+			throw new IllegalArgumentException("cipher should be instance of DJCiphertext");
+		}
+		
+		DJCiphertext djCipher = (DJCiphertext) cipher;
+		//n is the modulus in the public key.
+		//Calculate s = (|cipher| -1) / |n|
+		int s = ((djCipher).getCipher().bitLength() - 1) / publicKey.getModulus().bitLength();
+
+		//Calculate N and N' based on s: N = n^s, N' = n^(s+1)
+		BigInteger n = publicKey.getModulus();
+		BigInteger N = n.pow(s);
+		BigInteger Ntag = n.pow(s+1);
+		
+		//Make sure the cipher belongs to ZN'
+		if(djCipher.getCipher().compareTo(BigInteger.ZERO) < 0 || djCipher.getCipher().compareTo(Ntag) >= 0)
+			throw new IllegalArgumentException("The cipher is not in ZN'");
+			
+		BigInteger NtagMinus1 = Ntag.subtract(BigInteger.ONE);
+		//Choose a random r in ZNtag*, this can be done by choosing a random value between 1 and Ntag -1 
+		//which is with overwhelming probability in Zntag*
+		BigInteger r = BigIntegers.createRandomInRange(BigInteger.ONE, NtagMinus1, random);
+		BigInteger c = djCipher.getCipher().multiply(r.modPow(N, Ntag));
+		
+		return new DJCiphertext(c);
+	}
+
+	/**
+	 * Given two ciphers c1 = Enc(p1)  and c2 = Enc(p2) this function return c1 + c2 = Enc(p1 +p2).
+	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymAdditiveHomomorphicEnc#add(edu.biu.scapi.midLayer.ciphertext.Ciphertext, edu.biu.scapi.midLayer.ciphertext.Ciphertext)
+	 */
+	@Override
+	public Ciphertext add(Ciphertext cipher1, Ciphertext cipher2) {
+		//ciphertext should be Damgard-Jurik ciphertext
+		if (!(cipher1 instanceof DJCiphertext) || !(cipher1 instanceof DJCiphertext)){
+			throw new IllegalArgumentException("cipher should be instance of DJCiphertext");
+		}
+		DJCiphertext djCipher1 = (DJCiphertext) cipher1;
+		DJCiphertext djCipher2 = (DJCiphertext) cipher2;
+		//n is the modulus in the public key.
+		//Calculate s = (|cipher| -1) / |n|
+		int s1 = ((djCipher1).getCipher().bitLength() - 1) / publicKey.getModulus().bitLength();
+		int s2 = ((djCipher2).getCipher().bitLength() - 1) / publicKey.getModulus().bitLength();
+		if(s1 != s2){
+			throw new ScapiRuntimeException("Sizes of ciphertexts do not match");
+		}
+		
+		//Calculate N and N' based on s: N = n^s, N' = n^(s+1)
+		BigInteger n = publicKey.getModulus();
+		BigInteger N = n.pow(s1);
+		BigInteger Ntag = n.pow(s1+1);
+		
+		//Check that cipher1 and cipher2 belong to ZN'
+		if(djCipher1.getCipher().compareTo(BigInteger.ZERO) < 0 || djCipher1.getCipher().compareTo(Ntag) >= 0)
+			throw new IllegalArgumentException("cipher1 is not in ZN'");
+		if(djCipher2.getCipher().compareTo(BigInteger.ZERO) < 0 || djCipher2.getCipher().compareTo(Ntag) >= 0)
+			throw new IllegalArgumentException("cipher2 is not in ZN'");
+		
+		BigInteger c1 = djCipher1.getCipher();
+		BigInteger c2 = djCipher2.getCipher();
+		BigInteger c = c1.multiply(c2).mod(Ntag);
+		
+		BigInteger NtagMinus1 = Ntag.subtract(BigInteger.ONE);
+		//Choose a random r in ZNtag*, this can be done by choosing a random value between 1 and Ntag -1 
+		//which is with overwhelming probability in Zntag*
+		BigInteger r = BigIntegers.createRandomInRange(BigInteger.ONE, NtagMinus1, random);
+		c = c.multiply(r.modPow(N, Ntag));
+		
+		return new DJCiphertext(c);
+	}
+
+	/**
+	 * This function calculates the homomorphic multiplication by a constant of a ciphertext<p>
+	 * in the Damgard Jurik encryption scheme.
+	 * @param cipher the cipher to operate on
+	 * @param constNumber the constant number by which to multiply the cipher
+	 * @throws IllegalArgumentException if the cipher is not an instance of DJCiphertext
+	 */
+	@Override
+	public Ciphertext multByConst(Ciphertext cipher, BigInteger constNumber) {
+		//ciphertext should be Damgard-Jurik ciphertext
+		if (!(cipher instanceof DJCiphertext)){
+			throw new IllegalArgumentException("cipher should be instance of DJCiphertext");
+		}
+		
+		DJCiphertext djCipher = (DJCiphertext) cipher;
+		//n is the modulus in the public key.
+		//Calculate s = (|cipher| -1) / |n|
+		int s = ((djCipher).getCipher().bitLength() - 1) / publicKey.getModulus().bitLength();
+
+		//Calculate N and N' based on s: N = n^s, N' = n^(s+1)
+		BigInteger n = publicKey.getModulus();
+		BigInteger N = n.pow(s);
+		BigInteger Ntag = n.pow(s+1);
+		
+		//Make sure the cipher belongs to ZN'
+		if(djCipher.getCipher().compareTo(BigInteger.ZERO) < 0 || djCipher.getCipher().compareTo(Ntag) >= 0)
+			throw new IllegalArgumentException("The cipher is not in ZN'");
+		
+		//Make sure the constant number belongs to ZN
+		if(constNumber.compareTo(BigInteger.ZERO) < 0 || constNumber.compareTo(N) >= 0)
+			throw new IllegalArgumentException("The constant number is not in ZN");
+	
+		BigInteger c = djCipher.getCipher().modPow(constNumber, Ntag);
+		
+		BigInteger NtagMinus1 = Ntag.subtract(BigInteger.ONE);
+		//Choose a random r in ZNtag*, this can be done by choosing a random value between 1 and Ntag -1 
+		//which is with overwhelming probability in Zntag*
+		BigInteger r = BigIntegers.createRandomInRange(BigInteger.ONE, NtagMinus1, random);
+		c = c.multiply(r.modPow(N, Ntag));
+		
+		return new DJCiphertext(c);
+	}
+	
+	//This function generates a value d such that d = 1 mod N and d = 0 mod t, using the Chinese Remainder Theorem.
 	private BigInteger generateD(BigInteger N, BigInteger t){
 		Vector<BigInteger> congruences = new Vector<BigInteger>();
 		congruences.add(BigInteger.ONE);
 		congruences.add(BigInteger.ZERO);
 		Vector<BigInteger> moduli = new Vector<BigInteger>();
 		moduli.add(N);
-		moduli.add(privateKey.getT());
+		moduli.add(t);
 		BigInteger d = MathAlgorithms.chineseRemainderTheorem(congruences, moduli);
 		return d;
 	}
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#generateKey(java.security.spec.AlgorithmParameterSpec)
-	 */
-	@Override
-	public KeyPair generateKey(AlgorithmParameterSpec keyParams)throws InvalidParameterSpecException {
-		if(!(keyParams instanceof DJKeyGenParameterSpec)){
-			throw new InvalidParameterSpecException("keyParamas has to be an instance of DJKeyGenParameterSpec");
-		}
-		int certainty = 40;
-		DJKeyGenParameterSpec params = (DJKeyGenParameterSpec)keyParams;
-		RSAModulus rsaMod = ScRSAPermutation.generateRSAModulus(params.getModulusLength(), params.getCertainty(), random);
-		
-		BigInteger pMinus1 = rsaMod.p.subtract(BigInteger.ONE);
-		BigInteger qMinus1 = rsaMod.q.subtract(BigInteger.ONE);
-		BigInteger gcdPMinus1QMinus1 = pMinus1.gcd(qMinus1);
-		BigInteger t = (pMinus1.multiply(qMinus1)).divide(gcdPMinus1QMinus1);
-		BigInteger dForS1 = generateD(rsaMod.n, t); //precalculate d for the case that s == 1
-		
-		return new KeyPair(new ScDamgardJurikPublicKey(rsaMod.n), new ScDamgardJurikPrivateKey(t, dForS1));
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#generateKey()
-	 */
-	@Override
-	public KeyPair generateKey() {
-		throw new UnsupportedOperationException("Use generateKey function with DJKeyGenParameterSpec");
-	}
-
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.DamgardJurikPaillierEnc#reRandomize(edu.biu.scapi.midLayer.ciphertext.Ciphertext)
-	 */
-	@Override
-	public Ciphertext reRandomize(Ciphertext cipher) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymAdditiveHomomorphicEnc#add(edu.biu.scapi.midLayer.ciphertext.Ciphertext, edu.biu.scapi.midLayer.ciphertext.Ciphertext)
-	 */
-	@Override
-	public Ciphertext add(Ciphertext cipher1, Ciphertext cipher2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymAdditiveHomomorphicEnc#multByConst(edu.biu.scapi.midLayer.ciphertext.Ciphertext, java.math.BigInteger)
-	 */
-	@Override
-	public Ciphertext multByConst(Ciphertext cipher1, BigInteger constNumber) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 }
+
