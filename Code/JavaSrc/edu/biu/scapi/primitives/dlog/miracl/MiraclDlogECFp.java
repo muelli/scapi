a77964ab84ca31dab551c8bc21d69fd8e21ec55b
Moriya Farbstein <moriyaw@gmail.com>
2012-05-03 09:04:02 +0000

    merged with NoInitParams branch. added a call to the optimized algorithm of exponentiateWithPreComputedValues, which is implemented in miracl dll.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1202 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/miracl/MiraclDlogECFp.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/miracl/MiraclDlogECFp.java
@@ -29,8 +29,10 @@ public class MiraclDlogECFp extends MiraclAdapterDlogEC implements DlogECFp, DDH
 	private native boolean validateFpGenerator(long mip, long generator, byte[] x, byte[] y);
 	private native boolean isFpMember(long mip, long point);
 	private native long createInfinityFpPoint(long mip);
-	//private native long exponentiateFpWithPreComputed(long mip, long exponentiationsMap, long base, int bits, byte[] size);
+	private native long createECFpObject(long mip, byte[] p, byte[] a, byte[] b);
+	private native long exponentiateFpWithPreComputed(long mip, long dlogGroup, long base, byte[] size, int window, int maxBits);
 	
+	private long nativeDlog = 0;
 	
 	
 	public void init(String curveName) throws IllegalArgumentException{
@@ -190,6 +192,15 @@ public class MiraclDlogECFp extends MiraclAdapterDlogEC implements DlogECFp, DDH
 		
 	}
 	
+	/**
+	 * Computes the product of several exponentiations with distinct bases 
+	 * and distinct exponents. 
+	 * Instead of computing each part separately, an optimization is used to 
+	 * compute it simultaneously. 
+	 * @param groupElements
+	 * @param exponentiations
+	 * @return the exponentiation result
+	 */
 	@Override
 	public GroupElement simultaneousMultipleExponentiations(GroupElement[] groupElements, 
 			BigInteger[] exponentiations) throws UnInitializedException{
@@ -227,11 +238,11 @@ public class MiraclDlogECFp extends MiraclAdapterDlogEC implements DlogECFp, DDH
 			(GroupElement groupElement, BigInteger exponent) throws UnInitializedException{
 		
 		//tests showed that the naive algorithm is faster than the optimized.
-		return exponentiate(groupElement, exponent);
+		//return exponentiate(groupElement, exponent);
 		
 		//override of the function exponentiateWithPreComputedValues that uses the same algorithm as the ABS but in native.
 		//Results showed that the naive algorithm is faster so we dicide not to use this algorithm but the naive
-		/*if (!isInitialized()){
+		if (!isInitialized()){
 			throw new UnInitializedException();
 		}
 		//if the GroupElements don't match the DlogGroup, throw exception
@@ -246,16 +257,16 @@ public class MiraclDlogECFp extends MiraclAdapterDlogEC implements DlogECFp, DDH
 			return base;
 		}
 		
-		if (exponentiationsMap == 0){
-			exponentiationsMap = createExponentiationsMap();
-			System.out.println("created map");
+		if (nativeDlog == 0){
+			ECFpGroupParams params = (ECFpGroupParams) getGroupParams();
+			nativeDlog = createECFpObject(mip, params.getP().toByteArray(), params.getA().mod(params.getP()).toByteArray(), params.getB().toByteArray());
 		}
 		
 		//call to native exponentiate function
-		long result = exponentiateFpWithPreComputed(mip, exponentiationsMap, base.getPoint(), ((ECFpGroupParams) groupParams).getP().bitLength()+1, exponent.toByteArray());
-		System.out.println("java result is "+result);
+		long result = exponentiateFpWithPreComputed(mip, nativeDlog, base.getPoint(), exponent.toByteArray(), getWindow(), getOrder().bitLength());
+
 		//build a ECF2mPointMiracl element from the result value
-		return new ECFpPointMiracl(result, this);*/
+		return new ECFpPointMiracl(result, this);
 	}
 	
 	/**
