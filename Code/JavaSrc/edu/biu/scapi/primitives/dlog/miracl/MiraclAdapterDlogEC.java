658373bd8ff435d64c8118fe7684b5c0cd184e0d
Moriya Farbstein <moriyaw@gmail.com>
2012-05-03 09:54:47 +0000

    merged with NoInitParams branch. added window size, which used in the optimized algorithm of exponentiateWithPreComputedValues.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1204 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/miracl/MiraclAdapterDlogEC.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/miracl/MiraclAdapterDlogEC.java
@@ -2,12 +2,14 @@ package edu.biu.scapi.primitives.dlog.miracl;
 
 
 
+import edu.biu.scapi.exceptions.UnInitializedException;
 import edu.biu.scapi.primitives.dlog.DlogEllipticCurve;
 import edu.biu.scapi.primitives.dlog.DlogGroupEC;
 
 public abstract class MiraclAdapterDlogEC extends DlogGroupEC 
 										  implements DlogEllipticCurve{
 	
+	protected int window = 0;
 	private native long createMip();
 	//protected native long createExponentiationsMap();
 	
@@ -24,5 +26,21 @@ public abstract class MiraclAdapterDlogEC extends DlogGroupEC
 		return mip;
 	}
 	
+	public void setWindow(int val){
+		window = val;
+	}
+	
+	protected int getWindow() throws UnInitializedException{
+		if (window != 0){
+			return window;
+		}
+		int bits = getOrder().bitLength();
+		if (bits <= 256){
+			window =  8;
+		} else {
+			window = 10;
+		}
+		return window;
+	}
 }
 
