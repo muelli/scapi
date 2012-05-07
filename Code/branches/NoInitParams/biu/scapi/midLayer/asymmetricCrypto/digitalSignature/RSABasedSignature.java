4266d39c9ad133289bc6d152d7563258ba422af0
Yael Ejgenberg <yaelnegje@yahoo.com>
2012-05-07 10:35:32 +0000

    All the hierarchy under edu/biu/... has been moved from SDK/code/branches/NoInitParams to here. Eventually SDK/code/branches/NoInitParams will be removed.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK@1250 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/branches/NoInitParams/biu/scapi/midLayer/asymmetricCrypto/digitalSignature/RSABasedSignature.java
+++
@@ -1,8 +0,0 @@
-package edu.biu.scapi.midLayer.asymmetricCrypto.digitalSignature;
-
-import edu.biu.scapi.securityLevel.ACMA;
-import edu.biu.scapi.securityLevel.UnlimitedTimes;
-
-public interface RSABasedSignature extends DigitalSignature, UnlimitedTimes, ACMA{
-
-}
--- Code/branches/NoInitParams/biu/scapi/midLayer/asymmetricCrypto/keys/ElGamalPrivateKey.java
+++
@@ -1,9 +0,0 @@
-package edu.biu.scapi.midLayer.asymmetricCrypto.keys;
-
-import java.math.BigInteger;
-import java.security.PrivateKey;
-
-public interface ElGamalPrivateKey extends PrivateKey {
-	
-	public BigInteger getX();
-}
